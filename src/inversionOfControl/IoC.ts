9b0860db957bdba310106c77586b4620d93421d2
Khusamov Sukhrob <khusamov@yandex.ru>
2022-03-30 16:33:43 +0300

    В вызов resolver() добавлен IResolverContext последним аргументом

--- src/inversionOfControl/IoC.ts
+++ src/inversionOfControl/IoC.ts
@@ -1,15 +1,19 @@
 import createRegistratorResolver from './createRegistratorResolver';
-
-export type TDependencyMap = Map<string, TResolverFunction>
-export type TResolverFunction = (...args: Array<any>) => any
+import IResolverContext from './IResolverContext';
+import {TDependencyMap} from './types';
 
 /**
- * Inversion Of Control Container.
+ * Контейнер инверсии управления (Inversion оf Control Container).
+ * @link https://bit.ly/35sJch2
  */
 export default class IoC {
 	private static readonly registratorDependencyName = 'Registrator'
 	private dependencyMap: TDependencyMap = new Map()
 
+	/**
+	 * Конструктор контейнера IoC.
+	 * Автоматически добавляется зависимость Registrator.
+	 */
 	public constructor() {
 		this.dependencyMap.set(
 			IoC.registratorDependencyName,
@@ -17,14 +21,22 @@ export default class IoC {
 		)
 	}
 
+	/**
+	 * Разрешение зависимости.
+	 * TODO Сделать проверку соответствия типов аргументов args из resolve() и resolver(),
+	 * TODO Сделать проверку соответствия выходного типа resolver() и запрашиваемого типа resolve<T>.
+	 */
 	public resolve<T>(dependencyName: string, ...args: Array<any>): T {
-		// TODO Сделать проверку соответствия типов аргументов args и resolver,
-		//  а также соответствие выходного типа и запрашиваемого типа.
 		const resolver = this.dependencyMap.get(dependencyName)
 		if (!resolver) {
 			throw new Error(`Не найдена зависимость '${dependencyName}'`)
 		}
-		return resolver(...args)
+		return resolver(...args, this.getContext())
+	}
+
+	private getContext(): IResolverContext {
+		return {
+			iocContainer: this
+		}
 	}
 }
-
