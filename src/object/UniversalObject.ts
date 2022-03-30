d636d89c48b71202e4d12ded1ec8af575e2f73bf
Khusamov Sukhrob <khusamov@yandex.ru>
2022-03-30 17:26:04 +0300

    Добавлен интерфейс IUniversalObject

--- src/object/UniversalObject.ts
+++ src/object/UniversalObject.ts
@@ -1,14 +1,7 @@
-export default class UniversalObject {
+import IUniversalObject from '../types/IUniversalObject';
+
+export default class UniversalObject implements IUniversalObject {
 	#map: Map<string, any> = new Map
-
-	public delete(name: string) {
-		// Обычно, это не проблема - задача delete, чтобы значения с данным ключом не было,
-		// поэтому, если до вызова delete ключа не было, то задача уже выполнена
-		// https://github.com/khusamov/solid-futurio-ru/pull/1#discussion_r813171547
-		if (!this.#map.has(name)) return;
-
-		this.#map.delete(name)
-	}
 
 	public setValue(name: string, value: any) {
 		this.#map.set(name, value)
