cecdc8e4319b6c598a5dcd09bf1822128110691d
Khusamov Sukhrob <khusamov@yandex.ru>
2022-03-30 16:35:04 +0300

    Пример запуска программы приведен в актуальное состояние

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,24 @@ import adapterSourceGenerator from './inversionOfControl/adapterGenerator/adapte
 const iocContainer = new IoC()
 
 iocContainer.resolve<IRegistrator>('Registrator', 'Adapter', adapterGenerator).register()
-iocContainer.resolve<IRegistrator>('Registrator', 'IMovable.position.getter', (universalObject: UniversalObject): number => {
-	return universalObject.getValue('position')
-}).register()
+iocContainer.resolve<IRegistrator>(
+	'Registrator',
+	'IMovable.position.getter',
+	(universalObject: UniversalObject): number => {
+		return universalObject.getValue('position')
+	}
+).register()
 
 const uObject = new UniversalObject()
 uObject.setValue('position', 100)
-const movableAdapter = iocContainer.resolve<IMovable>('Adapter', reflect<IMovable>(), uObject, iocContainer)
-console.log('movableAdapter.position',movableAdapter.position)
+const movableAdapter = iocContainer.resolve<IMovable>('Adapter', reflect<IMovable>(), uObject)
 
 const bodyElement = document.querySelector('body')
 if (bodyElement) {
 	const preElement = bodyElement.appendChild(document.createElement('pre'))
 	preElement.style.tabSize = String(3)
 	preElement.innerText = adapterSourceGenerator(reflect<IMovable>())
+
+	const pElement = bodyElement.appendChild(document.createElement('p'))
+	pElement.innerText = `movableAdapter.position = ${movableAdapter.position}`
 }
