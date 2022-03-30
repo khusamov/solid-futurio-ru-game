302258e17fb5a578f675a5b91370077b140537c9
Khusamov Sukhrob <khusamov@yandex.ru>
2022-03-30 16:34:49 +0300

    Добавлен поддержка IResolverContext функцией adapterGenerator()

--- src/inversionOfControl/adapterGenerator/adapterGenerator.ts
+++ src/inversionOfControl/adapterGenerator/adapterGenerator.ts
@@ -2,13 +2,13 @@ import {ReflectedTypeRef} from 'typescript-rtti';
 import adapterSourceGenerator from './adapterSourceGenerator';
 import UniversalObject from '../../object/UniversalObject';
 import execFunctionScript from '../../functions/execFunctionScript';
+import IResolverContext from '../IResolverContext';
 import IAdapterClass from './IAdapterClass';
-import IoC from '../IoC';
 
 export default (
-	function adapterGenerator(reflectedTypeRef: ReflectedTypeRef, universalObject: UniversalObject, iocContainer: IoC) {
+	function adapterGenerator(reflectedTypeRef: ReflectedTypeRef, universalObject: UniversalObject, context: IResolverContext) {
 		const source = adapterSourceGenerator(reflectedTypeRef)
 		const AdapterClass = execFunctionScript<IAdapterClass>(source)
-		return new AdapterClass(universalObject, iocContainer)
+		return new AdapterClass(universalObject, context.iocContainer)
 	}
 )
