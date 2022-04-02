eaea249874705d6f59ae9d0b454bf5de11a6c96b
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-02 22:25:37 +0300

    Добавлен экспорт IGetApplicationRollupConfigOptions

--- packages/khusamov-rollup-config/src/index.ts
+++ packages/khusamov-rollup-config/src/index.ts
@@ -1,4 +1,5 @@
 export {default as getApplicationRollupConfig} from './application/getApplicationRollupConfig'
 export {default as getLibraryRollupConfig} from './library/getLibraryRollupConfig'
 
+export {default as IGetApplicationRollupConfigOptions} from './application/IGetApplicationRollupConfigOptions'
 export {default as IGetLibraryRollupConfigOptions} from './library/IGetLibraryRollupConfigOptions'
