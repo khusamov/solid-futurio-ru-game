868e9e36ae9e4c600f39f3294c3b12d3f01035c7
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-02 22:27:15 +0300

    Добавлена зависимость от khusamov-inversion-of-control

--- packages/khusamov-inversion-of-control-sample/src/index.ts
+++ packages/khusamov-inversion-of-control-sample/src/index.ts
@@ -1,10 +1,10 @@
 import 'reflect-metadata'
 import {reflect} from 'typescript-rtti'
-import {IoC} from './inversionOfControl';
-import {adapterGeneratorResolver, adapterSourceGenerator} from './adapterGenerator';
-import UniversalObject from './object/UniversalObject';
+import {IoC, IRegistrator, UniversalObject} from 'khusamov-inversion-of-control';
+import {adapterGeneratorResolver, adapterSourceGenerator} from 'khusamov-inversion-of-control';
 import formatCode from './formatCode';
-import IRegistrator from './types/IRegistrator';
+
+const {querySelector, createElement} = document
 
 const iocContainer = new IoC()
 iocContainer.resolve<IRegistrator>('Registrator', 'Adapter', adapterGeneratorResolver).register()
@@ -20,12 +20,12 @@ movableAdapter.position = 100
 
 const adapterSource = adapterSourceGenerator(reflect<IMovable>())
 
-const bodyElement = document.querySelector('body')
+const bodyElement = querySelector('body')
 if (bodyElement) {
-	const preElement = bodyElement.appendChild(document.createElement('pre'))
+	const preElement = bodyElement.appendChild(createElement('pre'))
 	preElement.style.tabSize = String(3)
 	preElement.innerText = formatCode(adapterSource)
 
-	const pElement = bodyElement.appendChild(document.createElement('p'))
+	const pElement = bodyElement.appendChild(createElement('p'))
 	pElement.innerText = `movableAdapter.position = ${movableAdapter.position}`
 }
