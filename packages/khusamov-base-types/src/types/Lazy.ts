67b4433dd6dd12f0759a6bead60456426f85cb75
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-05 00:47:06 +0300

    Добавлен класс Lazy

---
+++ packages/khusamov-base-types/src/types/Lazy.ts
@@ -0,0 +1,26 @@
+import ICreator from './ICreator';
+
+/**
+ * Отложенная инициализация.
+ * @link https://metanit.com/sharp/tutorial/20.1.php
+ * @link https://docs.microsoft.com/ru-ru/dotnet/api/system.lazy-1?view=net-5.0
+ */
+export default class Lazy<T> {
+	#value: T | undefined
+	#creator: ICreator<T>
+
+	constructor(private createFn: () => T) {
+		this.#creator = {
+			create(): T {
+				return createFn()
+			}
+		}
+	}
+
+	get value(): T {
+		if (!this.#value) {
+			this.#value = this.#creator.create()
+		}
+		return this.#value
+	}
+}
