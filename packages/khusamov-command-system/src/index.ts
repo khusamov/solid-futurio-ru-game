17d762d61c791bfdb1321d97f74a5e16af3ae88b
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-05 16:27:01 +0300

    Исправлены экспорты для команд Старт/Стоп

--- packages/khusamov-command-system/src/index.ts
+++ packages/khusamov-command-system/src/index.ts
@@ -1,7 +1,5 @@
-export {default as RepeatCommand} from './command/RepeatCommand'
 export {default as NotOperationCommand} from './command/NotOperationCommand'
-export {default as MacroCommand} from './command/MacroCommand'
 export {default as BridgeCommand} from './command/BridgeCommand'
-
-export {default as StartCommand, IStartable} from './command/StartCommand'
-export {default as StopCommand, IStopable} from './command/StopCommand'
+export {default as MacroCommand} from './command/MacroCommand'
+export {default as RepeatCommand} from './command/RepeatCommand'
+export {StartCommand, StopCommand, IStartStopObject} from './command/StartStop'
