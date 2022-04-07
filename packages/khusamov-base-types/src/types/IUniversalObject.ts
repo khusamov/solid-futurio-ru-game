02cd01c58f2bc728f87195eb6d6a004aee2b0070
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-07 21:39:29 +0300

    Тип значений IUniversalObject изменен на any

--- packages/khusamov-base-types/src/types/IUniversalObject.ts
+++ packages/khusamov-base-types/src/types/IUniversalObject.ts
@@ -5,8 +5,8 @@ export interface IItem<K, V> {
 	value: V | undefined
 }
 
-export default interface IUniversalObject<V> {
-	getValue(name: TValueName): V | undefined
-	setValue(name: TValueName, value: V): void
-	readonly items: IItem<TValueName, V>[]
+export default interface IUniversalObject {
+	getValue<V>(name: TValueName): V | undefined
+	setValue<V>(name: TValueName, value: V): void
+	readonly items: IItem<TValueName, any>[]
 }
