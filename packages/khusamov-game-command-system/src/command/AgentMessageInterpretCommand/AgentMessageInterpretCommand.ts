faecd3adf9add3bc8daf79fd7ae7a49e1d2acd65
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-12 18:05:13 +0300

    Мелкие исправления AgentMessageInterpretCommand

--- packages/khusamov-game-command-system/src/command/AgentMessageInterpretCommand/AgentMessageInterpretCommand.ts
+++ packages/khusamov-game-command-system/src/command/AgentMessageInterpretCommand/AgentMessageInterpretCommand.ts
@@ -1,25 +1,28 @@
 import {reflect} from 'typescript-rtti';
 import {ICommand, IQueue, IUniversalObject} from 'khusamov-base-types';
-import {IoC} from 'khusamov-inversion-of-control';
+import {resolve} from 'khusamov-inversion-of-control';
+import {NotOperationCommand, repeatable} from 'khusamov-command-system';
 import IAgentMessage from './IAgentMessage';
-import {NotOperationCommand} from 'khusamov-command-system';
 
-type TAgentMessageQueue = IQueue<IUniversalObject<any>>
+type TAgentMessageQueue = IQueue<IUniversalObject>
 
 /**
  * Интерпретация сообщений от клиентов.
  * https://stepik.org/lesson/664251/step/1?unit=662137
  */
+@repeatable
 export default class AgentMessageInterpretCommand implements ICommand {
 	execute(): void {
 		let agentCommand: ICommand = new NotOperationCommand
 
-		const agentMessageQueue = IoC.resolve<TAgentMessageQueue>('Agent.MessageQueue')
-		const agentMessageObject = agentMessageQueue.dequeue()
+		const agentMessageQueue = resolve<TAgentMessageQueue | undefined>('Agent.MessageQueue')
+		if (agentMessageQueue) {
+			const agentMessageObject = agentMessageQueue.dequeue()
 
-		if (agentMessageObject) {
-			const message = IoC.resolve<IAgentMessage>('Adapter', agentMessageObject, reflect<IAgentMessage>())
-			agentCommand = IoC.resolve<ICommand>(message.type, agentMessageObject)
+			if (agentMessageObject) {
+				const message = resolve<IAgentMessage>('Adapter', agentMessageObject, reflect<IAgentMessage>())
+				agentCommand = resolve<ICommand>(message.type, agentMessageObject)
+			}
 		}
 
 		agentCommand.execute()
