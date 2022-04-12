f32f07a020733734f1091a4ecf8c8c23d6de3e0c
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-12 20:52:40 +0300

    Добавлен экспорт класса AgentMessageInterpretCommand

---
+++ packages/khusamov-game-command-system/src/command/AgentMessageInterpretCommand/index.ts
@@ -0,0 +1,2 @@
+export {default} from './AgentMessageInterpretCommand'
+export {default as IAgentMessage} from './IAgentMessage'
--- packages/khusamov-game-command-system/src/index.ts
+++ packages/khusamov-game-command-system/src/index.ts
@@ -1,2 +1,3 @@
 export {IMovable, MoveCommand} from './command/MoveCommand'
 export {DecreaseForceAngleCommand, DecreaseForceCommand, IncreaseForceAngleCommand, IncreaseForceCommand} from './command/MoveCommand'
+export {default as AgentMessageInterpretCommand, IAgentMessage} from './command/AgentMessageInterpretCommand'
