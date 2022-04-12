6acfe51299c106f2e03340cd426e901a4360131e
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-12 20:52:11 +0300

    Удалена agentMessageInterpretCommandResolver()

--- packages/khusamov-game-command-system/src/command/AgentMessageInterpretCommand/agentMessageInterpretCommandResolver.ts
+++
@@ -1,7 +0,0 @@
-import {ICommand} from 'khusamov-base-types';
-import {RepeatableCommand} from 'khusamov-command-system';
-import AgentMessageInterpretCommand from './AgentMessageInterpretCommand';
-
-export default function agentMessageInterpretCommandResolver(): ICommand {
-	return new RepeatableCommand(new AgentMessageInterpretCommand)
-}
