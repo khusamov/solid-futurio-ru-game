f4c7b7673651a77a1271b152fa91d5af5769c788
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-12 17:42:21 +0300

    Добавлен тип TStoppableKey

--- packages/khusamov-command-system/src/command/RepeatableCommand/IObjectWithStoppable.ts
+++ packages/khusamov-command-system/src/command/RepeatableCommand/IObjectWithStoppable.ts
@@ -1,5 +1,16 @@
 import {IStoppable} from 'khusamov-base-types';
 
+/**
+ * Ключ к останавливаемой команде. Уникальное имя команды.
+ *
+ * Сейчас это просто строка, например имя команды.
+ * А можно было бы сделать ссылкой на класс команды, но сейчас экземпляр
+ * команды создается не напрямую, а косвенно через функцию resolve()
+ * в классе StartCommand (targetCommand = resolve(...)).
+ * Поэтому в качестве ключа возможно использовать только строку.
+ */
+export type TStoppableKey = string
+
 export default interface IObjectWithStoppable {
-	stoppableCommandMap: Map<string, IStoppable> | undefined
+	stoppableCommandMap: Map<TStoppableKey, IStoppable> | undefined
 }
