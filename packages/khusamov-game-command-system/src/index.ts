5722b30953be6a6e9b2aa324b1ce0ddbd6708457
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-14 20:37:13 +0300

    Класс AgentMessageInterpretCommand перенесен в пакет khusamov-command-system

--- packages/khusamov-game-command-system/src/index.ts
+++ packages/khusamov-game-command-system/src/index.ts
@@ -1,3 +1,2 @@
 export {IMovable, MoveCommand} from './command/MoveCommand'
 export {ITransformForceAgentMessage, TransformForceCommand, transformForceResolver} from './command/MoveCommand'
-export {default as AgentMessageInterpretCommand, IAgentMessage} from './command/AgentMessageInterpretCommand'
