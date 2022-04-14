773a4de2aeef8cc1ba5356d28db169182ee2e7a6
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-14 16:35:12 +0300

    Уточнены аргументы функции register()

--- packages/khusamov-inversion-of-control/src/inversionOfControl/register.ts
+++ packages/khusamov-inversion-of-control/src/inversionOfControl/register.ts
@@ -1,6 +1,7 @@
-import IoC from './IoC';
 import {IRegistrator} from 'khusamov-base-types';
+import {TResolverFunction} from './types';
+import IoC from './IoC';
 
-export default function register(...args: Array<any>) {
-	return IoC.resolve<IRegistrator>('Registrator', ...args).register()
+export default function register(dependencyName: string, resolver: TResolverFunction) {
+	return IoC.resolve<IRegistrator>('Registrator', dependencyName, resolver).register()
 }
