dddc65fd3bdb2b9656d2a4b0d9fb959e082f59e8
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-14 20:06:38 +0300

    Класс UniversalObject перенесен в пакет khusamov-base-types

--- packages/khusamov-inversion-of-control/src/index.ts
+++ packages/khusamov-inversion-of-control/src/index.ts
@@ -1,5 +1,4 @@
 export {default as IoC} from './inversionOfControl/IoC'
-export {default as UniversalObject} from './object/UniversalObject'
 
 export {default as adapterGeneratorResolver} from './adapterGenerator/adapterGeneratorResolver'
 export {default as adapterSourceGenerator} from './adapterGenerator/adapterSourceGenerator'
