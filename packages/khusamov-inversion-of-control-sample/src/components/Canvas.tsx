a050fe094b0d7dbd0bebfd19ea3932e89ab61268
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-14 16:36:31 +0300

    Стилизация кода

--- packages/khusamov-inversion-of-control-sample/src/components/Canvas.tsx
+++ packages/khusamov-inversion-of-control-sample/src/components/Canvas.tsx
@@ -1,14 +1,14 @@
-import styles from './Canvas.module.scss'
 import {SVGProps} from 'react';
+import styles from './Canvas.module.scss'
 
 const data = 'M 10 10 L 100 100'
 
 const props: SVGProps<SVGSVGElement> = {
 	version: '1.1',
 	xmlns: 'http://www.w3.org/2000/svg',
+	className: styles.Canvas,
 	width: 500,
-	height: 500,
-	className: styles.Canvas
+	height: 500
 }
 
 export default function Canvas() {
