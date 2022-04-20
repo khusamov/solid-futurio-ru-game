7c3f8f56d00ece6f16f383ad89d0420f6d4f1149
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-20 15:11:43 +0300

    Добавлена поддержка Node.js для пакета khusamov-format-code

--- packages/khusamov-format-code/src/formatCode.ts
+++ packages/khusamov-format-code/src/formatCode.ts
@@ -1,14 +1,19 @@
-import {format} from 'prettier/standalone'
-import {Options} from 'prettier'
+import {format as browserFormat} from 'prettier/standalone'
+import {Options, format as nodeFormat} from 'prettier'
 import IPrettierPlugins from './IPrettierPlugins'
 import prettierOptions from './prettier.options.json'
 
-const globalWindow = window as never as Window & IPrettierPlugins
+const isBrowser = new Function('try { return this === window } catch (e) { return false }')()
+const format = isBrowser ? browserFormat : nodeFormat
 
 const formatOptions = {
 	parser: 'typescript',
-	plugins: globalWindow.prettierPlugins,
 	...prettierOptions as Options
+}
+
+if (isBrowser) {
+	const globalWindow = window as never as Window & IPrettierPlugins
+	formatOptions.plugins = globalWindow.prettierPlugins
 }
 
 /**
--- packages/khusamov-format-code/src/index.ts
+++ packages/khusamov-format-code/src/index.ts
@@ -1,1 +1,1 @@
-export {default} from './formatCode'
+export {default as formatCode} from './formatCode'
