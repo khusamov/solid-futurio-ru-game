0c9db6ddab5cf2407e13b82c65f7bbfc11030f6d
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-21 17:25:16 +0300

    Выделена функция generateProjectAdapters()

---
+++ packages/khusamov-adapter-generator/src/functions/generateProjectAdapters.ts
@@ -0,0 +1,87 @@
+import {
+	getJSDocTags, isPropertySignature, SyntaxKind,
+	factory, NodeFlags, createPrinter, EmitHint, Statement, ClassElement
+} from 'typescript'
+import {formatCode} from 'khusamov-format-code';
+import adapterFactory from '../factory/adapterFactory';
+import getProgram from './getProgram';
+import getComment from './getComment';
+import getInterfaceDeclarations from './getInterfaceDeclarations';
+import {getImportInfoFromPropertySignature, removeImportInfoDuplicates} from './parseImportDeclaration';
+
+export default function generateProjectAdapters(projectPath: string) {
+	// Загрузка проекта в память.
+	const program = getProgram(projectPath, 'src/index.ts')
+	const projectSourceFiles = program.getSourceFiles().filter(sourceFile => sourceFile.fileName.startsWith(projectPath))
+	const foundInterfaceDeclarations = getInterfaceDeclarations(projectSourceFiles)
+
+	// Получение всех интерфейсов, помеченных тегами adaptable в JSDoc-блоках.
+	const adaptableInterfaceDeclarations = (
+		foundInterfaceDeclarations.filter(interfaceDeclaration => {
+			return !!getJSDocTags(interfaceDeclaration).find(tag => tag.tagName.text === 'adaptable')
+		})
+	)
+
+	// Получение списка файлов с исходниками классов адаптеров и путей для сохранения.
+	const adapterInfoList = (
+		adaptableInterfaceDeclarations.map(interfaceDeclaration => {
+			const statements: Statement[] = []
+
+			const propertySignatures = interfaceDeclaration.members.filter(isPropertySignature)
+
+			// Импорты типов свойств интерфейса.
+			const importInfoList = removeImportInfoDuplicates(propertySignatures.map(getImportInfoFromPropertySignature))
+			for (const importInfo of importInfoList) {
+				if (importInfo) {
+					statements.push(importInfo.importDeclaration)
+				}
+			}
+
+			// Комментарий к классу.
+			const comment = getComment(interfaceDeclaration.name, program)
+			if (comment) {
+				statements.push(factory.createJSDocComment(comment) as Statement)
+			}
+
+			// Определение класса.
+			statements.push(
+				adapterFactory.createClassDeclaration(
+					interfaceDeclaration,
+					propertySignatures.reduce<ClassElement[]>((result, propertySignature) => {
+						const comment = getComment(propertySignature.name, program)
+						if (comment) {
+							result.push(factory.createJSDocComment(comment) as ClassElement)
+						}
+						result.push(adapterFactory.createGetAccessorDeclaration(propertySignature))
+						result.push(adapterFactory.createSetAccessorDeclaration(propertySignature))
+						return result
+					}, [])
+				)
+			)
+			return {
+				fileName: adapterFactory.getAdapterFilename(interfaceDeclaration),
+				sourceFile: (
+					factory.createSourceFile(
+						statements,
+						factory.createToken(SyntaxKind.EndOfFileToken),
+						NodeFlags.None
+					)
+				)
+			}
+		})
+	)
+
+	for (const adapterInfo of adapterInfoList) {
+		console.log('Путь к файлу:', adapterInfo.fileName.replace(projectPath, ''))
+		console.log(
+			formatCode(
+				createPrinter({omitTrailingSemicolon: true})
+					.printNode(
+						EmitHint.Unspecified,
+						adapterInfo.sourceFile,
+						adapterInfo.sourceFile
+					)
+			)
+		)
+	}
+}
--- packages/khusamov-adapter-generator/src/index.ts
+++ packages/khusamov-adapter-generator/src/index.ts
@@ -1,18 +1,7 @@
 import {resolve} from 'path'
 import {readdir, stat} from 'fs/promises'
-import {
-	getJSDocTags,
-	isPropertySignature,
-	SyntaxKind,
-	factory, NodeFlags, createPrinter, EmitHint, Statement, ClassElement
-} from 'typescript'
-import getProgram from './functions/getProgram';
-import getInterfaceDeclarations from './functions/getInterfaceDeclarations';
-import adapterFactory from './factory/adapterFactory';
-import getComment from './functions/getComment';
-import {getImportInfoFromPropertySignature, removeImportInfoDuplicates} from './functions/parseImportDeclaration';
-import {formatCode} from 'khusamov-format-code';
 import {program as commanderProgram} from 'commander'
+import generateProjectAdapters from './functions/generateProjectAdapters';
 
 if (require.main === module) {
 	generate()
@@ -38,83 +27,6 @@ export async function generate() {
 	for (const projectDirectory of projectDirectories) {
 		console.log('- - - - - - - - - - - - - - - - - - - - - - - - - -')
 		console.log('Генерация адаптеров для проекта:', projectDirectory)
-		generateForProject(resolve(options.directoryOfProjects, projectDirectory))
-	}
-}
-
-function generateForProject(projectPath: string) {
-	// Загрузка проекта в память.
-	const program = getProgram(projectPath, 'src/index.ts')
-	const projectSourceFiles = program.getSourceFiles().filter(sourceFile => sourceFile.fileName.startsWith(projectPath))
-	const foundInterfaceDeclarations = getInterfaceDeclarations(projectSourceFiles)
-
-	// Получение всех интерфейсов, помеченных тегами adaptable в JSDoc-блоках.
-	const adaptableInterfaceDeclarations = (
-		foundInterfaceDeclarations.filter(interfaceDeclaration => {
-			return !!getJSDocTags(interfaceDeclaration).find(tag => tag.tagName.text === 'adaptable')
-		})
-	)
-
-	// Получение списка файлов с исходниками классов адаптеров и путей для сохранения.
-	const adapterInfoList = (
-		adaptableInterfaceDeclarations.map(interfaceDeclaration => {
-			const statements: Statement[] = []
-
-			const propertySignatures = interfaceDeclaration.members.filter(isPropertySignature)
-
-			// Импорты типов свойств интерфейса.
-			const importInfoList = removeImportInfoDuplicates(propertySignatures.map(getImportInfoFromPropertySignature))
-			for (const importInfo of importInfoList) {
-				if (importInfo) {
-					statements.push(importInfo.importDeclaration)
-				}
-			}
-
-			// Комментарий к классу.
-			const comment = getComment(interfaceDeclaration.name, program)
-			if (comment) {
-				statements.push(factory.createJSDocComment(comment) as Statement)
-			}
-
-			// Определение класса.
-			statements.push(
-				adapterFactory.createClassDeclaration(
-					interfaceDeclaration,
-					propertySignatures.reduce<ClassElement[]>((result, propertySignature) => {
-						const comment = getComment(propertySignature.name, program)
-						if (comment) {
-							result.push(factory.createJSDocComment(comment) as ClassElement)
-						}
-						result.push(adapterFactory.createGetAccessorDeclaration(propertySignature))
-						result.push(adapterFactory.createSetAccessorDeclaration(propertySignature))
-						return result
-					}, [])
-				)
-			)
-			return {
-				fileName: adapterFactory.getAdapterFilename(interfaceDeclaration),
-				sourceFile: (
-					factory.createSourceFile(
-						statements,
-						factory.createToken(SyntaxKind.EndOfFileToken),
-						NodeFlags.None
-					)
-				)
-			}
-		})
-	)
-
-	for (const adapterInfo of adapterInfoList) {
-		console.log('Путь к файлу:', adapterInfo.fileName.replace(projectPath, ''))
-		console.log(
-			formatCode(
-				createPrinter({omitTrailingSemicolon: true})
-					.printNode(
-						EmitHint.Unspecified,
-						adapterInfo.sourceFile,
-						adapterInfo.sourceFile
-					)
-			)
-		)
+		generateProjectAdapters(resolve(options.directoryOfProjects, projectDirectory))
 	}
 }
