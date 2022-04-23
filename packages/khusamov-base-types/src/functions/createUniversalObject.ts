6b01567c254858a4bd2107683ae73b2081916f3c
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-23 21:45:27 +0300

    Возвращаемый тип функции createUniversalObject() исправлен на IUniversalObject

--- packages/khusamov-base-types/src/functions/createUniversalObject.ts
+++ packages/khusamov-base-types/src/functions/createUniversalObject.ts
@@ -1,10 +1,11 @@
 import UniversalObject from '../types/UniversalObject';
+import IUniversalObject from '../types/IUniversalObject';
 
 /**
  * Создать UniversalObject на основе plane-объекта.
  * @param object
  */
-export default function createUniversalObject<T extends object>(object: T): UniversalObject {
+export default function createUniversalObject<T extends object>(object: T): IUniversalObject {
 	const result = new UniversalObject
 	for (const key in object) {
 		if (object.hasOwnProperty(key)) {
