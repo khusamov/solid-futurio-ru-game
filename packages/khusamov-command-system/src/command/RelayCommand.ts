3ce5f4a8a6bd6062385f1dcf7900a185dd9a04ff
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-23 12:11:50 +0300

    Аргументы в конструкторе класса RelayCommand поменяны местами

--- packages/khusamov-command-system/src/command/RelayCommand.ts
+++ packages/khusamov-command-system/src/command/RelayCommand.ts
@@ -1,11 +1,15 @@
 import {ICommand} from 'khusamov-base-types';
 
+export type TAction = () => void
+
 /**
+ * Команда с пользовательской функцией.
  * @link https://metanit.com/sharp/wpf/22.3.php
  * @link https://docs.microsoft.com/en-us/windows/communitytoolkit/mvvm/relaycommand
  */
 export default class RelayCommand implements ICommand {
 	readonly #name: string | undefined
+	private readonly action: TAction
 
 	public get name(): string {
 		const prefix = 'RelayCommand'
@@ -14,17 +18,26 @@ export default class RelayCommand implements ICommand {
 
 	/**
 	 * Конструктор команды с пользовательской функцией.
+	 * @param nameOrAction Имя команды или пользовательская функция.
 	 * @param action Пользовательская функция.
-	 * @param name Имя команды.
 	 */
-	constructor(
-		private action: () => void,
-		name?: string
-	) {
-		this.#name = name
+	constructor(nameOrAction: string | TAction, action?: TAction) {
+		const [_name, _action] = parseConstructorArguments(nameOrAction, action)
+		this.#name = _name
+		this.action = _action
 	}
 
 	execute(): void {
 		this.action()
 	}
+}
+
+function parseConstructorArguments(nameOrAction: string | TAction, action?: TAction) {
+	return (
+		(
+			arguments.length === 1
+				? [undefined, action]
+				: [nameOrAction, action]
+		) as [string | undefined, TAction]
+	)
 }
