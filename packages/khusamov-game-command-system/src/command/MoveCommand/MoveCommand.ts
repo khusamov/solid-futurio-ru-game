e145b4949678d2ea1c5907a9324344c789ada00a
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-23 21:51:11 +0300

    Метод add заменен на translate

--- packages/khusamov-game-command-system/src/command/MoveCommand/MoveCommand.ts
+++ packages/khusamov-game-command-system/src/command/MoveCommand/MoveCommand.ts
@@ -15,8 +15,8 @@ export default class MoveCommand implements ICommand {
 	public execute(): void {
 		const {mass, position, linearAcceleration, linearVelocity, appliedForce} = this.movable
 		this.movable.linearAcceleration = appliedForce.scale(1 / mass)
-		this.movable.linearVelocity = linearVelocity.add(linearAcceleration.scale(this.timeInterval))
-		this.movable.position = position.add(linearVelocity)
+		this.movable.linearVelocity = linearVelocity.translate(linearAcceleration.scale(this.timeInterval))
+		this.movable.position = position.translate(linearVelocity)
 	}
 
 	private get timeInterval(): number {
