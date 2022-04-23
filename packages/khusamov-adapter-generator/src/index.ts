664c516f835ab503df012c71cbba644c2509bdf7
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-23 11:13:47 +0300

    Позже продолжить разработку генератора адаптеров

--- packages/khusamov-adapter-generator/src/index.ts
+++ packages/khusamov-adapter-generator/src/index.ts
@@ -1,4 +1,5 @@
 import {resolve} from 'path'
+import {writeFile} from 'fs/promises'
 import {readdir, stat} from 'fs/promises'
 import {program as commanderProgram} from 'commander'
 import generateProjectAdapters from './functions/generateProjectAdapters';
@@ -30,6 +31,7 @@ export async function generate() {
 		const generateResult = generateProjectAdapters(resolve(options.directoryOfProjects, projectDirectory))
 		for (const info of generateResult) {
 			// TODO Сделать сохранение файлов адаптеров.
+			//console.log('info.sourceFile.getText()',info.sourceFile.getText())
 		}
 	}
 }
