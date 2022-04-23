e34e6e093c4067a9000b176fe5584e1f47c7a058
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-23 11:37:24 +0300

    Из хука убрана вся инициализация игры (хук ведь вызывается при каждой повторной отрисовки компонент)

--- packages/khusamov-inversion-of-control-sample/src/components/Application/useApplication.ts
+++ packages/khusamov-inversion-of-control-sample/src/components/Application/useApplication.ts
@@ -3,84 +3,81 @@ import {adapterGeneratorResolver, register} from 'khusamov-inversion-of-control'
 import {AgentMessageInterpretCommand, CommandQueue, IStopAgentMessage, RepeatablePlugin, stopCommandResolver} from 'khusamov-command-system';
 import {IMovable, ITransformForceAgentMessage, transformForceResolver} from 'khusamov-game-command-system';
 
-export default function useApplication() {
-	register('Adapter', adapterGeneratorResolver)
-	register('StopCommand', stopCommandResolver)
-	register('TransformForce', transformForceResolver)
+register('Adapter', adapterGeneratorResolver)
+register('StopCommand', stopCommandResolver)
+register('TransformForce', transformForceResolver)
 
-	const universalObjectList: UniversalObject[] = []
-	universalObjectList.push(
-		createUniversalObject<IMovable & {name: string}>({
-			name: 'theSpaceship',
-			time: 0,
-			mass: 1000,
-			position: new Vector(0, 0),
-			appliedForce: new Vector(0, 0),
-			linearVelocity: new Vector(0, 0),
-			linearAcceleration: new Vector(0, 0)
-		})
-	)
+const keyUpDownProcessor = new KeyUpDownProcessor
+const agentMessageQueue: Queue<IUniversalObject> = new Queue
+const universalObjectList: UniversalObject[] = []
+const commandQueue = new CommandQueue({plugins: [new RepeatablePlugin]})
 
-	register('GameObject', (name: string): IUniversalObject | undefined => {
-		return universalObjectList.find(object => object.getValue('name') === name)
+register('Agent.MessageQueue', (): Queue<IUniversalObject> => agentMessageQueue)
+register('GameObject', (name: string): IUniversalObject | undefined => {
+	return universalObjectList.find(object => object.getValue('name') === name)
+})
+
+commandQueue.enqueue(new AgentMessageInterpretCommand)
+
+const gameTimer = new Timer(1000, () => {
+	const command = commandQueue.dequeue()
+	if (command) {
+		console.log(gameTimer.interval, command.name)
+		command.execute()
+	}
+})
+
+universalObjectList.push(
+	createUniversalObject<IMovable & {name: string}>({
+		name: 'theSpaceship',
+		time: 0,
+		mass: 1000,
+		position: new Vector(0, 0),
+		appliedForce: new Vector(0, 0),
+		linearVelocity: new Vector(0, 0),
+		linearAcceleration: new Vector(0, 0)
 	})
+)
 
-	const commandQueue = new CommandQueue
-	commandQueue.addPlugin(new RepeatablePlugin)
-
-	const keyUpDownProcessor = new KeyUpDownProcessor
-	const agentMessageQueue: Queue<IUniversalObject> = new Queue
-	const gameTimer = new Timer(1000, () => {
-		const command = commandQueue.dequeue()
-		if (command) {
-			console.log(gameTimer.interval, command.name)
-			command.execute()
+document.onkeydown = event => {
+	keyUpDownProcessor.onKeyDown(event, () => {
+		switch (event.code) {
+			case 'KeyW':
+				agentMessageQueue.enqueue(
+					createUniversalObject<ITransformForceAgentMessage>({
+						type: 'TransformForce',
+						targetObject: {
+							type: 'GameObject',
+							name: 'theSpaceship'
+						},
+						angle: 0,
+						scale: 1
+					})
+				)
+				break
 		}
 	})
-
-	register('Agent.MessageQueue', (): Queue<IUniversalObject> => agentMessageQueue)
-	commandQueue.enqueue(new AgentMessageInterpretCommand)
-
-	gameTimer.start()
-
-
-
-	document.onkeydown = event => {
-		keyUpDownProcessor.onKeyDown(event, () => {
-			switch (event.code) {
-				case 'KeyW':
-					agentMessageQueue.enqueue(
-						createUniversalObject<ITransformForceAgentMessage>({
-							type: 'TransformForce',
-							targetObject: {
-								type: 'GameObject',
-								name: 'theSpaceship'
-							},
-							angle: 0,
-							scale: 1
-						})
-					)
-					break
-			}
-		})
-	}
-
-	document.onkeyup = event => {
-		keyUpDownProcessor.onKeyUp(event, () => {
-			switch (event.code) {
-				case 'KeyW':
-					agentMessageQueue.enqueue(
-						createUniversalObject<IStopAgentMessage>({
-							type: 'StopCommand',
-							commandName: 'TransformForce',
-							targetObject: {
-								type: 'GameObject',
-								name: 'theSpaceship'
-							}
-						})
-					)
-					break
-			}
-		})
-	}
 }
+
+document.onkeyup = event => {
+	keyUpDownProcessor.onKeyUp(event, () => {
+		switch (event.code) {
+			case 'KeyW':
+				agentMessageQueue.enqueue(
+					createUniversalObject<IStopAgentMessage>({
+						type: 'StopCommand',
+						commandName: 'TransformForce',
+						targetObject: {
+							type: 'GameObject',
+							name: 'theSpaceship'
+						}
+					})
+				)
+				break
+		}
+	})
+}
+
+gameTimer.start()
+
+export default function useApplication() {}
