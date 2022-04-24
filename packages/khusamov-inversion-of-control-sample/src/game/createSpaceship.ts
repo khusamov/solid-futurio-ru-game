1749f322ae80354b6ac8fe920a42654caf2d4596
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-24 13:48:01 +0300

    В функцию createSpaceship() добавлен необязательный аргумент params

--- packages/khusamov-inversion-of-control-sample/src/game/createSpaceship.ts
+++ packages/khusamov-inversion-of-control-sample/src/game/createSpaceship.ts
@@ -1,16 +1,20 @@
-import {createUniversalObject, Vector} from 'khusamov-base-types';
+import {createUniversalObject, IUniversalObject, Vector} from 'khusamov-base-types';
 import {IMovable} from 'khusamov-game-command-system';
+import IGameObject from './IGameObject';
 
-export default function createSpaceship() {
-	return (
-		createUniversalObject<IMovable & {name: string}>({
-			name: 'theSpaceship',
-			time: 0,
-			mass: 1000,
-			position: new Vector(0, 0),
-			appliedForce: new Vector(0, 0),
-			linearVelocity: new Vector(0, 0),
-			linearAcceleration: new Vector(0, 0)
-		})
-	)
+interface ISpaceship extends IGameObject, IMovable {}
+
+const defaultSpaceship: ISpaceship = {
+	name: 'theSpaceship',
+	time: 0,
+	mass: 1000,
+	position: new Vector(0, 0),
+	appliedForce: new Vector(0, 0),
+	linearVelocity: new Vector(0, 0),
+	linearAcceleration: new Vector(0, 0)
 }
+
+export default function createSpaceship(params: Partial<ISpaceship> = {}): IUniversalObject {
+	return createUniversalObject<ISpaceship>(Object.assign({}, defaultSpaceship, params))
+}
+
