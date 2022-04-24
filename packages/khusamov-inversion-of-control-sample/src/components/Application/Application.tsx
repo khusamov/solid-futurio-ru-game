2bd6036c3600b0b0d4aa90d62bc0acda44ae4e6d
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-24 21:00:55 +0300

    Исправлен возврат отрисовки компонента Application

--- packages/khusamov-inversion-of-control-sample/src/components/Application/Application.tsx
+++ packages/khusamov-inversion-of-control-sample/src/components/Application/Application.tsx
@@ -14,49 +14,49 @@ interface IParam {
 export default function Application() {
 	const [game] = useGame({renderTimeout: 10, gameTimeout: 10})
 
-	if (game) {
-		const theSpaceshipObject = findGameObjectByName(game.gameObjectList, 'theSpaceship')
-		const theSpaceship = theSpaceshipObject ? resolve<IMovable>('Adapter', theSpaceshipObject, IMovableReflectedTypeRef) : undefined
+	if (!game) return null
 
-		if (theSpaceship) {
-			const params: IParam[] = [{
-				title: 'Время',
-				value: new Date(game.gameTimer.interval).getMinutes() + ':' + new Date(game.gameTimer.interval).getSeconds(),
-				unit: ''
-			}, {
-				title: 'Координаты',
-				value: theSpaceship.position.toString(),
-				unit: 'Метры'
-			}, {
-				title: 'Скорость',
-				value: theSpaceship.linearVelocity.toString(),
-				unit: 'Метры в секунду'
-			}, {
-				title: 'Ускорение',
-				value: theSpaceship.linearAcceleration.toString(),
-				unit: 'Метры в секунду в квадрате'
-			}, {
-				title: 'Сила',
-				value: `${theSpaceship.appliedForce.length.toFixed(2)}, ${Angle.toDegree(theSpaceship.appliedForce.angle).toFixed(2)}`,
-				unit: 'Ньютон, градус'
-			}]
+	const theSpaceshipObject = findGameObjectByName(game.gameObjectList, 'theSpaceship')
+	const theSpaceship = theSpaceshipObject ? resolve<IMovable>('Adapter', theSpaceshipObject, IMovableReflectedTypeRef) : undefined
 
-			return (
-				<div>
-					<div>
-						<table>
-							{params.map(param => (
-								<tr>
-									<td>{param.title}:</td>
-									<td>{param.value}</td>
-									<td>{param.unit}</td>
-								</tr>
-							))}
-						</table>
-						<Canvas theSpaceshipPosition={theSpaceship.position}/>
-					</div>
-				</div>
-			)
-		}
-	}
+	if (!theSpaceship) return null
+
+	const params: IParam[] = [{
+		title: 'Время',
+		value: new Date(game.gameTimer.interval).getMinutes() + ':' + new Date(game.gameTimer.interval).getSeconds(),
+		unit: ''
+	}, {
+		title: 'Координаты',
+		value: theSpaceship.position.toString(),
+		unit: 'Метры'
+	}, {
+		title: 'Скорость',
+		value: theSpaceship.linearVelocity.toString(),
+		unit: 'Метры в секунду'
+	}, {
+		title: 'Ускорение',
+		value: theSpaceship.linearAcceleration.toString(),
+		unit: 'Метры в секунду в квадрате'
+	}, {
+		title: 'Сила',
+		value: `${theSpaceship.appliedForce.length.toFixed(2)}, ${Angle.toDegree(theSpaceship.appliedForce.angle).toFixed(2)}`,
+		unit: 'Ньютон, градус'
+	}]
+
+	return (
+		<div>
+			<div>
+				<table>
+					{params.map(param => (
+						<tr>
+							<td>{param.title}:</td>
+							<td>{param.value}</td>
+							<td>{param.unit}</td>
+						</tr>
+					))}
+				</table>
+				<Canvas theSpaceshipPosition={theSpaceship.position}/>
+			</div>
+		</div>
+	)
 }
