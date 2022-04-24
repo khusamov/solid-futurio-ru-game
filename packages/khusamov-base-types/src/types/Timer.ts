284df7d25c7cc18ad196f66a2922a75154c8560b
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-24 13:45:51 +0300

    В действие Timer.action добавлен this со ссылкой на сам таймер

--- packages/khusamov-base-types/src/types/Timer.ts
+++ packages/khusamov-base-types/src/types/Timer.ts
@@ -1,18 +1,25 @@
 import IStartable from './IStartable';
 import IStoppable from './IStoppable';
 
+export type TAction = (this: Timer) => void
+
 export default class Timer implements IStartable, IStoppable {
 	private timerId: number | undefined
 	private time: Date | undefined
 
+	/**
+	 * Внимание, внутри action определена this как Timer.
+	 * @param timeout
+	 * @param action
+	 */
 	constructor(
 		private timeout: number,
-		private action: () => void
+		private action: TAction
 	) {}
 
 	public start() {
 		if (this.timerId === undefined) {
-			this.timerId = window.setInterval(this.action, this.timeout)
+			this.timerId = window.setInterval(this.action.bind(this), this.timeout)
 			this.time = new Date
 		}
 	}
