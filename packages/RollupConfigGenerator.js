d9ce154957a9e3102b57dec401dd38e5f0403c86
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-26 14:07:55 +0300

    Добавлена ссылка github.com/rollup/awesome

--- packages/RollupConfigGenerator.js
+++ packages/RollupConfigGenerator.js
@@ -1,5 +1,6 @@
 
 // https://github.com/rollup/plugins
+// https://github.com/rollup/awesome
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
