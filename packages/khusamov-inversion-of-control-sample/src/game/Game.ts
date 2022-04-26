01c4f7a4e0abb286c17d76bc5a59cc11a1c671d7
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-26 15:03:21 +0300

    Добавлена корректировка координат для тороидального пространства

--- packages/khusamov-inversion-of-control-sample/src/game/Game.ts
+++ packages/khusamov-inversion-of-control-sample/src/game/Game.ts
@@ -1,11 +1,12 @@
 import init from './init';
-import {IUniversalObject, KeyUpDownProcessor, Queue, Timer, Vector} from 'khusamov-base-types';
+import {ISize, IUniversalObject, KeyUpDownProcessor, Queue, Timer, Vector} from 'khusamov-base-types';
 import {AgentMessageInterpretCommand, CommandQueue, StartCommand} from 'khusamov-command-system';
-import createSpaceship from './createSpaceship';
+import createSpaceshipObject from './createSpaceshipObject';
 import createKeyboardHandlers from './createKeyboardHandlers';
-import {IMovable, IMovableReflectedTypeRef, MoveCommand} from 'khusamov-game-command-system';
+import {IMovable, IMovableReflectedTypeRef, MoveCommand, MoveCorrectionCommand} from 'khusamov-game-command-system';
 import {resolve} from 'khusamov-inversion-of-control';
 import IGameOptions from './IGameOptions';
+import positionCorrectionForToroid from './positionCorrectionForToroid';
 
 export default class Game {
 	public gameTimer: Timer
@@ -13,6 +14,7 @@ export default class Game {
 	public commandQueue: CommandQueue
 	public agentMessageQueue: Queue<IUniversalObject>
 	public keyUpDownProcessor: KeyUpDownProcessor
+	public size: ISize = {width: 0, height: 0}
 
 	public constructor(options: IGameOptions) {
 		const {gameTimer, gameObjectList, commandQueue, agentMessageQueue, keyUpDownProcessor} = init(options)
@@ -22,9 +24,10 @@ export default class Game {
 		this.agentMessageQueue = agentMessageQueue
 		this.keyUpDownProcessor = keyUpDownProcessor
 
-		const theSpaceship = createSpaceship()
-		gameObjectList.push(theSpaceship)
-		commandQueue.enqueue(createSpaceshipStartMoveCommand(theSpaceship))
+		const theSpaceshipObject = createSpaceshipObject({position: new Vector(500, 200)})
+		gameObjectList.push(theSpaceshipObject)
+		commandQueue.enqueue(createSpaceshipStartMoveCommand(theSpaceshipObject))
+		commandQueue.enqueue(createSpaceshipStartMoveCorrectionCommand(theSpaceshipObject, () => this.size))
 
 		commandQueue.enqueue(new AgentMessageInterpretCommand)
 		createKeyboardHandlers(keyUpDownProcessor, agentMessageQueue)
@@ -47,16 +50,32 @@ export default class Game {
 /**
  * Создаем команду Поступательное движение космолета.
  * TODO Может это упростить? Создание абстрактной команды StartCommand.
- * @param theSpaceship
  */
 function createSpaceshipStartMoveCommand(theSpaceship: IUniversalObject) {
 	return (
 		new StartCommand(
-			new MoveCommand(
+			new MoveCommand(resolve<IMovable>('Adapter', theSpaceship, IMovableReflectedTypeRef)),
+			'MoveSpaceshipCommand',
+			theSpaceship
+		)
+	)
+}
+
+/**
+ * TODO Возможно стоит сделать команду MoveToroidCorrectionCommand и включить ее в khusamov-game-command-system
+ * @param theSpaceship
+ * @param getSize
+ */
+function createSpaceshipStartMoveCorrectionCommand(theSpaceship: IUniversalObject, getSize: () => ISize) {
+	return (
+		new StartCommand(
+			new MoveCorrectionCommand(
 				resolve<IMovable>('Adapter', theSpaceship, IMovableReflectedTypeRef),
-				new Vector(500, 500)
+				movable => {
+					movable.position = positionCorrectionForToroid(movable.position, getSize)
+				}
 			),
-			'MoveCommand',
+			'MoveCorrectionSpaceshipCommand',
 			theSpaceship
 		)
 	)
