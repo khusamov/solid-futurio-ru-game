4cbc0966cd7ac197e534a06b18282d57cd3fa652
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-27 20:54:38 +0300

    Добавлена функция isStoppable()

--- packages/khusamov-base-types/src/types/IStoppable.ts
+++ packages/khusamov-base-types/src/types/IStoppable.ts
@@ -1,3 +1,7 @@
 export default interface IStoppable {
 	stop(): void
 }
+
+export function isStoppable(object: any): object is IStoppable {
+	return 'stop' in object && typeof object.stop === 'function'
+}
