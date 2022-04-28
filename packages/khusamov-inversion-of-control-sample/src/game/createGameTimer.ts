17bf1db367419c527f422e7148cadbe7eeaa4fb8
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-28 16:05:19 +0300

    Сделана отладочная версия игрового таймера

--- packages/khusamov-inversion-of-control-sample/src/game/createGameTimer.ts
+++ packages/khusamov-inversion-of-control-sample/src/game/createGameTimer.ts
@@ -1,14 +1,36 @@
-import {ICommand, IQueue, Timer} from 'khusamov-base-types';
+import {Timer} from 'khusamov-base-types';
+import {TCommandQueue} from './types';
+
+const DEBUG_DEFAULT = false
 
 /**
  * Создать игровой таймер.
  * Это бесконечный цикл по извлечению и выполнению команд из очереди команд.
- * @param timeout
- * @param commandQueue
  */
-export default function createGameTimer(timeout: number, commandQueue: IQueue<ICommand>): Timer {
-	return new Timer(
-		timeout,
-		() => commandQueue.dequeue()?.execute()
+export default function createGameTimer(timeout: number, commandQueue: TCommandQueue, debug: boolean = DEBUG_DEFAULT): Timer {
+	return (
+		debug
+			? (
+				new Timer(
+					timeout,
+					() => {
+						const command = commandQueue.dequeue()
+						if (command) {
+							console.log(command.name)
+							command.execute()
+						}
+					}
+				)
+			)
+			: ( // TODO Переделать выполнение команд. Надо чтобы следующий execute() сразу выполнялся после предыдущего.
+				//  А тут возможны даже наложения, когда execute() не закончился, а уже вызывается следующий execute().
+				// https://www.youtube.com/watch?v=-gsjAz9jR3Y
+				// https://eatdog.com.ua/assets/gamedev-slides/
+				// http://gs-studio.com/news-about-it/30703----javascript
+				new Timer(
+					timeout,
+					() => commandQueue.dequeue()?.execute()
+				)
+			)
 	)
 }
