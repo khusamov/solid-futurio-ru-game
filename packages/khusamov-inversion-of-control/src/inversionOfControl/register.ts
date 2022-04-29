caa58dd9d43ba6778e328ca44b48329839fc3c9d
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-29 15:39:44 +0300

    Вызов IoC.resolve() заменен на resolve()

--- packages/khusamov-inversion-of-control/src/inversionOfControl/register.ts
+++ packages/khusamov-inversion-of-control/src/inversionOfControl/register.ts
@@ -1,7 +1,7 @@
 import {IRegistrator} from 'khusamov-base-types';
 import {TResolverFunction} from './types';
-import IoC from './IoC';
+import resolve from './resolve';
 
 export default function register(dependencyName: string, resolver: TResolverFunction) {
-	return IoC.resolve<IRegistrator>('Registrator', dependencyName, resolver).register()
+	return resolve<IRegistrator>('Registrator', dependencyName, resolver).register()
 }
