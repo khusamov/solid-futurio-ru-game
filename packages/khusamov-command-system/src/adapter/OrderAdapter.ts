2bb750a7ea004d82420d3a6a33cf34e4af5125e4
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-29 21:38:30 +0300

    Добавлена ссылка на typescript-rtti/issues/60

--- packages/khusamov-command-system/src/adapter/OrderAdapter.ts
+++ packages/khusamov-command-system/src/adapter/OrderAdapter.ts
@@ -1,6 +1,12 @@
 import {IUniversalObject} from 'khusamov-universal-object';
 import {IOrder} from '../command/InterpretOrderCommand';
 
+// Нет возможности добраться до родительского интерфейса ITyped и в итоге свойство type недоступно для адаптера.
+// https://github.com/typescript-rtti/typescript-rtti/issues/60
+// const order = resolve<IOrder>('Adapter', orderObject, reflect<IOrder>())
+// Поэтому пришлось тут адаптер написать вручную.
+
+
 export default class OrderAdapter implements Partial<IOrder> {
 	constructor(private universalObject: IUniversalObject) {}
 
