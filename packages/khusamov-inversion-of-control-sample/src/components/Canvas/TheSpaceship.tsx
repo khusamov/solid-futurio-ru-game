7fd9d4665b2892f15727418f694d0bf98e4f6b12
Khusamov Sukhrob <khusamov@yandex.ru>
2022-04-30 14:00:31 +0300

    Добавлен вывод вектора скорости

--- packages/khusamov-inversion-of-control-sample/src/components/Canvas/TheSpaceship.tsx
+++ packages/khusamov-inversion-of-control-sample/src/components/Canvas/TheSpaceship.tsx
@@ -2,6 +2,7 @@ import {Fragment} from 'react';
 import {IMovable} from 'khusamov-game-command-system';
 import GameObjectRender from './GameObjectRender';
 import {Angle, ISize} from 'khusamov-base-types';
+import styles from './TheSpaceship.module.scss'
 
 const radius = 10
 
@@ -14,16 +15,27 @@ export default function TheSpaceship({theSpaceship, canvasSize: {width, height}}
 	return (
 		<Fragment>
 			<GameObjectRender position={theSpaceship.position} size={{width, height}}>
+				{({position}) => <circle className={styles.theSpaceship} cx={position.x} cy={position.y} r={radius}/>}
+			</GameObjectRender>
+			<GameObjectRender position={theSpaceship.position} size={{width, height}}>
 				{({position}) => (
 					<line
+						className={styles.appliedForce}
 						x1={0} y1={0}
-						x2={theSpaceship.appliedForce.length} y2={0}
+						x2={theSpaceship.appliedForce.length / 50} y2={0}
 						transform={`translate(${position.x} ${position.y}), rotate(${Angle.toDegree(theSpaceship.appliedForce.angle)})`}
 					/>
 				)}
 			</GameObjectRender>
 			<GameObjectRender position={theSpaceship.position} size={{width, height}}>
-				{({position}) => <circle cx={position.x} cy={position.y} r={radius}/>}
+				{({position}) => (
+					<line
+						className={styles.linearVelocity}
+						x1={0} y1={0}
+						x2={theSpaceship.linearVelocity.length} y2={0}
+						transform={`translate(${position.x} ${position.y}), rotate(${Angle.toDegree(theSpaceship.linearVelocity.angle)})`}
+					/>
+				)}
 			</GameObjectRender>
 		</Fragment>
 	)
