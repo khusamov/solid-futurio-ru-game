55846b135e2750ff803a0f904492e09b3c872415
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-01 10:05:56 +0300

    В классе GameLoop добавлена генерация событий вместо параметров update и render конструктора

--- packages/khusamov-base-types/src/types/GameLoop.ts
+++ packages/khusamov-base-types/src/types/GameLoop.ts
@@ -1,3 +1,4 @@
+import EventEmitter from 'events';
 import IStoppable from './IStoppable';
 import IStartable from './IStartable';
 
@@ -7,21 +8,36 @@ import IStartable from './IStartable';
  * https://www.youtube.com/watch?v=-gsjAz9jR3Y
  */
 export default class GameLoop implements IStartable, IStoppable {
-	private readonly fps = 60
-	private readonly step: number = 1 / this.fps
+	private readonly eventEmitter = new EventEmitter
+	private timeInterval: number = 0
+	private time: number = 0
 
-	private deltaTime: number = 0
-	private last: number = 0
-	private now: number = 0
+	/**
+	 * Временной шаг, с которым вызывается функция update для вычисления игровой логики.
+	 * Измеряется в миллисекундах.
+	 */
+	public get step(): number {
+		return 1 / this.framePerSecond / 1000
+	}
 
 	public constructor(
-		private update: (step: number) => void,
-		private render: (deltaTime: number) => void
+		/**
+		 * Планируемое число кадров в секунду.
+		 * @private
+		 */
+		private readonly framePerSecond = 60
 	) {}
 
+	on(eventName: 'update', update: (step: number) => void): this
+	on(eventName: 'render', render: (timeInterval: number) => void): this
+	on(eventName: string | symbol, listener: (...args: any[]) => void): this {
+		this.eventEmitter.on(eventName, listener)
+		return this
+	}
+
 	public start(): void {
-		this.deltaTime = 0
-		this.last = performance.now()
+		this.time = performance.now()
+		this.timeInterval = 0
 		requestAnimationFrame(this.frame.bind(this))
 	}
 
@@ -34,19 +50,19 @@ export default class GameLoop implements IStartable, IStoppable {
 	}
 
 	private frame() {
-		this.now = performance.now()
+		const currentTime = performance.now()
 
 		// Исправление проблемы неактивных вкладок.
 		// Разрешить максимальную задержку между вызовами не более, чем 1 секунда.
-		this.deltaTime = this.deltaTime + Math.min(1, (this.now - this.last) / 1000)
+		this.timeInterval = this.timeInterval + Math.min(1000, currentTime - this.time)
 
-		while(this.deltaTime > this.step) {
-			this.deltaTime = this.deltaTime - this.step
-			this.update(this.step)
+		while(this.timeInterval > this.step) {
+			this.timeInterval = this.timeInterval - this.step
+			this.eventEmitter.emit('update', this.step)
 		}
-		this.last = this.now
 
-		this.render(this.deltaTime)
+		this.time = currentTime
+		this.eventEmitter.emit('render', this.timeInterval)
 		requestAnimationFrame(this.frame.bind(this))
 	}
 }
