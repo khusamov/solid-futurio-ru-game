1c90516385414a5d548452f40be8e12252bc7b8e
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-01 10:24:09 +0300

    Глобальные регистрации перенесены из класса Game в register()

--- packages/khusamov-game/src/game/Game.ts
+++ packages/khusamov-game/src/game/Game.ts
@@ -1,20 +1,10 @@
-import {ICommand, IQueue, ISize, KeyUpDownProcessor, Queue} from 'khusamov-base-types';
-import {IUniversalObject} from 'khusamov-universal-object';
-import {createCommandQueue} from 'khusamov-command-system';
+import {ISize} from 'khusamov-base-types';
 import createTheSpaceshipKeyboardHandlers from './keyboardShortcut/createTheSpaceshipKeyboardHandlers';
 
 export default class Game {
-	public gameObjectList: Record<string, any>[]
-	public commandQueue: IQueue<ICommand>
-	public orderQueue: IQueue<IUniversalObject>
-	public keyUpDownProcessor: KeyUpDownProcessor
 	public worldSize: ISize = {width: 0, height: 0}
 
 	constructor() {
-		this.gameObjectList = []
-		this.commandQueue = createCommandQueue() // Очередь команд создается по особому!
-		this.orderQueue = new Queue
-		this.keyUpDownProcessor = new KeyUpDownProcessor
 		createTheSpaceshipKeyboardHandlers()
 	}
 }
--- packages/khusamov-game/src/game/register.ts
+++ packages/khusamov-game/src/game/register.ts
@@ -2,19 +2,33 @@ import {register} from 'khusamov-inversion-of-control';
 import {startMoveTransformCommandResolver} from './order/IMoveTransformOrder';
 import {stopCommandResolver} from './order/IStopOrder';
 import {MoveTransformCollection} from 'khusamov-mechanical-motion';
+import {IQueue, Queue} from 'khusamov-base-types';
+import {createCommandQueue} from 'khusamov-command-system';
 
 type TGameObject = Record<string, any>
+type TOrder = Record<string, any>
 
+// Глобальные объекты.
+
+const commandQueue = createCommandQueue() // Очередь команд создается по особому!
+const orderQueue: IQueue<TOrder> = new Queue
 const gameObjectList: TGameObject[] = []
 const moveTransformCollection = new MoveTransformCollection
 
+// Регистрации.
+
+register('Stop', stopCommandResolver)
+register('StartMoveTransform', startMoveTransformCommandResolver)
 register('GameObjectList', () => gameObjectList)
+register('OrderQueue', () => orderQueue)
+register('CommandQueue', () => commandQueue)
+
 register('GameObject', () => {
 	// TODO Дописать register GameObject
 	throw new Error('Зависимость GameObject не определена')
 })
-register('Stop', stopCommandResolver)
-register('StartMoveTransform', startMoveTransformCommandResolver)
+
 for (const transform of moveTransformCollection.transforms) {
+	// register MoveTransform:<Имя трансформации>
 	register(transform.name, transform.action)
 }
