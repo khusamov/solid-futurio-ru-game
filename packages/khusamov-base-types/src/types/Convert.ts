3abb2f3b4d2c44dcf25f632566110cf8c3fb70ee
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-01 10:06:33 +0300

    В функцию Convert.toArray() добавлена поддеержка plain-объектов

--- packages/khusamov-base-types/src/types/Convert.ts
+++ packages/khusamov-base-types/src/types/Convert.ts
@@ -1,20 +1,39 @@
 import IKeyValueObject from './IKeyValueObject';
 
 type TMapConvertResult<K, V> = IKeyValueObject<K, V | undefined>[]
+type TPlainObjectConvertResult<K, V> = IKeyValueObject<K, V>[]
+type TToArrayConvertResult<K, V> = IKeyValueObject<K, V>[]
 
 /**
  * Конвертация разнообразных значений.
  */
 export default class Convert {
 	/**
-	 * Конвертировать карту в массив key/value-объектов.
-	 * @param map
+	 * Конвертировать карту или plain-объект в массив key/value-объектов.
+	 * @param object
 	 */
-	public static toArray<K, V>(map: Map<K, V>): TMapConvertResult<K, V> {
-		const result: TMapConvertResult<K, V> = []
-		for (const [key, value] of map) {
-			result.push({key, value})
+	public static toArray<K, V>(object: Map<K, V>): TMapConvertResult<K, V>
+	public static toArray<K extends keyof any, V>(object: Record<K, V>): TPlainObjectConvertResult<K, V>
+	public static toArray<K extends keyof any, V>(object: Map<K, V> | Record<K, V>): TToArrayConvertResult<K, V> {
+		const result: TToArrayConvertResult<K, V> = []
+		if (object instanceof Map) {
+			for (const [key, value] of object) {
+				result.push({key, value})
+			}
+		} else {
+			for (const key in object) {
+				if (!object.hasOwnProperty(key)) continue
+				result.push({key, value: Reflect.get(object, key)})
+			}
 		}
 		return result
+	}
+
+	/**
+	 * Конвертировать миллисекунды в секунды.
+	 * @param millisecond
+	 */
+	public static toSecond(millisecond: number) {
+		return millisecond / 1000
 	}
 }
