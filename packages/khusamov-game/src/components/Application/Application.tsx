89cc679875bc09926bc0f918a78259ac235a267a
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-03 23:31:00 +0300

    Исправлен вывод movable

--- packages/khusamov-game/src/components/Application/Application.tsx
+++ packages/khusamov-game/src/components/Application/Application.tsx
@@ -1,10 +1,11 @@
-import styles from './Application.module.scss'
-import Canvas from '../Canvas';
-import useRequestAnimationFrame from '../../game/useRequestAnimationFrame';
 import {resolve} from 'khusamov-inversion-of-control';
+import {IMovable, MovableAdapter} from 'khusamov-mechanical-motion';
+import useRequestAnimationFrame from '../../game/useRequestAnimationFrame';
 import {TGameObjectList} from '../../game/types';
+import GameObjectAdapter from '../../game/gameObject/GameObjectAdapter';
+import Canvas from '../Canvas';
 import Movable from '../Movable';
-import {IMovable} from 'khusamov-mechanical-motion';
+import styles from './Application.module.scss'
 
 export default function Application() {
 	useRequestAnimationFrame()
@@ -12,9 +13,10 @@ export default function Application() {
 	return (
 		<div className={styles.Application}>
 			<Canvas>
-				{gameObjectList.map(gameObject => {
+				{gameObjectList.map(object => {
+					const gameObject = new GameObjectAdapter(object)
 					if (gameObject.kind && gameObject.kind.includes('IMovable')) {
-						return <Movable movable={gameObject as IMovable}/>
+						return <Movable movable={new MovableAdapter(object)}/>
 					}
 					return null
 				})}
