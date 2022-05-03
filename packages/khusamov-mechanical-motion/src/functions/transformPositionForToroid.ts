a2a93ce8b321317e864d0e1f4932325030ef343f
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-03 21:52:05 +0300

    Добавлена функция transformPositionForToroid()

---
+++ packages/khusamov-mechanical-motion/src/functions/transformPositionForToroid.ts
@@ -0,0 +1,21 @@
+import {ISize, Vector} from 'khusamov-base-types';
+
+/**
+ * Специальная функция для корректировки координат движущихся объектов
+ * с целью создания замкнутого пространства типа бублик.
+ * @param position
+ * @param toroidalSurfaceSize
+ */
+export default function transformPositionForToroid(position: Vector, {width, height}: ISize): Vector {
+	// Ограничить по положительной полуоси.
+	const result = position.mod(new Vector(width, height))
+	// Ограничить по отрицательной полуоси.
+	return (
+		result.translate(
+			new Vector(
+				result.x < 0 ? width : 0,
+				result.y < 0 ? height : 0
+			)
+		)
+	)
+}
