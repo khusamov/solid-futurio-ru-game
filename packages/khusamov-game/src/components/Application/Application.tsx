0a4d2b5fbd6eb9040fd04b575f8f3bff3f0c258a
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-03 21:54:02 +0300

    Добавлена предварительная реализация компонента Application

--- packages/khusamov-game/src/components/Application/Application.tsx
+++ packages/khusamov-game/src/components/Application/Application.tsx
@@ -1,9 +1,24 @@
 import styles from './Application.module.scss'
+import Canvas from '../Canvas';
+import useRequestAnimationFrame from '../../game/useRequestAnimationFrame';
+import {resolve} from 'khusamov-inversion-of-control';
+import {TGameObjectList} from '../../game/types';
+import Movable from '../Movable';
+import {IMovable} from 'khusamov-mechanical-motion';
 
 export default function Application() {
+	useRequestAnimationFrame()
+	const gameObjectList = resolve<TGameObjectList>('GameObjectList')
 	return (
 		<div className={styles.Application}>
-			Application
+			<Canvas>
+				{gameObjectList.map(gameObject => {
+					if (gameObject.kind && gameObject.kind.includes('IMovable')) {
+						return <Movable movable={gameObject as IMovable}/>
+					}
+					return null
+				})}
+			</Canvas>
 		</div>
 	)
 }
