190bf13e5ff6fedaa7dd7b7035ccf10046dbc06b
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-04 12:33:10 +0300

    Возврат к IUniversalObject от plain-объектов

--- packages/khusamov-game/src/game/order/IStopOrder.ts
+++ packages/khusamov-game/src/game/order/IStopOrder.ts
@@ -1,8 +1,7 @@
 import {IOrder, StopCommand} from 'khusamov-command-system';
 import {ICommand} from 'khusamov-base-types';
 import {resolve} from 'khusamov-inversion-of-control';
-import {IMovable} from 'khusamov-mechanical-motion';
-import {UniversalObjectAdapter} from 'khusamov-universal-object';
+import {IUniversalObject} from 'khusamov-universal-object';
 import {TTargetObjectSearchParams} from './IStartMoveTransformOrder';
 
 export default interface IStopOrder extends IOrder {
@@ -12,13 +11,10 @@ export default interface IStopOrder extends IOrder {
 }
 
 export function stopCommandResolver(order: IStopOrder): ICommand {
-	const target = resolve<IMovable | undefined>(order.targetObject.type, order.targetObject)
+	const target = resolve<IUniversalObject | undefined>(order.targetObject.type, order.targetObject)
 	if (!target) {
 		throw new Error(`Целевой объект не найден '${JSON.stringify(order.targetObject)}'`)
 	}
-	return new StopCommand(
-		order.command,
-		// TODO Избавиться от использования класса UniversalObjectAdapter
-		new UniversalObjectAdapter(target)
-	)
+
+	return new StopCommand(order.command, target)
 }
