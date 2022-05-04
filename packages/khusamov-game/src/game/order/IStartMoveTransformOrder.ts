cfa96cfdbc41fbab5a451548903a6ce4ac2b60e0
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-04 12:32:28 +0300

    Добавлен класс StartMoveTransformOrderAdapter

--- packages/khusamov-game/src/game/order/IStartMoveTransformOrder.ts
+++ packages/khusamov-game/src/game/order/IStartMoveTransformOrder.ts
@@ -18,7 +18,8 @@ export type TTransformActionParams = (
 	['DecreaseForce', ...Parameters<typeof decreaseForceActionResolver>] |
 	['ClockwiseRotateForce', ...Parameters<typeof clockwiseRotateForceActionResolver>] |
 	['CounterclockwiseRotateForce', ...Parameters<typeof counterclockwiseRotateForceActionResolver>] |
-	['ToroidalPositionTransformation', ...Parameters<typeof toroidalTransformActionResolver>]
+	['ToroidalPositionTransformation', ...Parameters<typeof toroidalTransformActionResolver>] |
+	['UnknownTransformAction']
 )
 
 export type TTargetObjectSearchParams = {
@@ -32,13 +33,43 @@ export default interface IStartMoveTransformOrder extends IOrder {
 	targetObject: TTargetObjectSearchParams
 }
 
-export function startMoveTransformCommandResolver(order: IStartMoveTransformOrder): ICommand {
-	const targetObject = resolve<IUniversalObject | undefined>(order.targetObject.type, order.targetObject)
-	if (!targetObject) {
-		throw new Error(`Целевой объект не найден '${JSON.stringify(order.targetObject)}'`)
+class StartMoveTransformOrderAdapter implements IStartMoveTransformOrder {
+	public readonly type = 'StartMoveTransform'
+	public constructor(private universalObject: IUniversalObject) {}
+
+	public get transformAction(): TTransformActionParams {
+		return this.universalObject.getValue('transformAction', ['UnknownTransformAction'])
 	}
 
-	const [transformName, ...transformParams] = order.transformAction
+	public set transformAction(value: TTransformActionParams) {
+		this.universalObject.setValue('transformAction', value)
+	}
+
+	public get targetObject(): TTargetObjectSearchParams {
+		return this.universalObject.getValue('targetObject', {
+			type: '',
+			name: ''
+		})
+	}
+
+	public set targetObject(value: TTargetObjectSearchParams) {
+		this.universalObject.setValue('targetObject', value)
+	}
+}
+
+export function startMoveTransformCommandResolver(startMoveTransformOrderObject: IUniversalObject): ICommand {
+	const startMoveTransformOrder = new StartMoveTransformOrderAdapter(startMoveTransformOrderObject)
+	const targetObject = (
+		resolve<IUniversalObject | undefined>(
+			startMoveTransformOrder.targetObject.type,
+			startMoveTransformOrder.targetObject
+		)
+	)
+	if (!targetObject) {
+		throw new Error(`Целевой объект не найден '${JSON.stringify(startMoveTransformOrder.targetObject)}'`)
+	}
+
+	const [transformName, ...transformParams] = startMoveTransformOrder.transformAction
 	const transformNamePrefix = 'MoveTransformAction.'
 
 	return (
