866d0c3675178d3ac9d13cebe471ef0e0fff6f2e
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-04 13:40:46 +0300

    В команду StopCommand добавлена возможность останавливать все команды объекта

--- packages/khusamov-command-system/src/command/StopCommand.ts
+++ packages/khusamov-command-system/src/command/StopCommand.ts
@@ -1,7 +1,5 @@
-import {reflect} from 'typescript-rtti';
 import {ICommand, IStoppable, IWithStoppable} from 'khusamov-base-types';
-import {IUniversalObject} from 'khusamov-universal-object';
-import {resolve} from 'khusamov-inversion-of-control';
+import {isUniversalObject, IUniversalObject} from 'khusamov-universal-object';
 import commandFullName from '../functions/commandFullName';
 import WithStoppableAdapter from '../adapter/WithStoppableAdapter';
 
@@ -9,12 +7,19 @@ import WithStoppableAdapter from '../adapter/WithStoppableAdapter';
  * Остановка команды, которую можно остановить.
  */
 export default class StopCommand implements ICommand {
+	private readonly stoppableCommandName: string | undefined
+	private readonly targetObject: IUniversalObject
+
 	public get name(): string {
 		return 'StopCommand: ' + this.stoppableCommandFullName
 	}
 
+	private get stoppableCommandFullName(): string {
+		return commandFullName(this.stoppableCommandName || 'All', this.stoppableCommand)
+	}
+
 	private get stoppableCommand(): ICommand & IStoppable | undefined {
-		return this.withStoppable.stoppableMap?.get(this.stoppableCommandName)
+		return this.stoppableCommandName ? this.withStoppable.stoppableMap?.get(this.stoppableCommandName) : undefined
 	}
 
 	private get withStoppable() {
@@ -23,29 +28,56 @@ export default class StopCommand implements ICommand {
 	}
 
 	/**
-	 * Конструктор.
+	 * Конструктор команды Стоп. Останавливаются все команды.
+	 * @param targetObject Объект, в котором будет производится поиск ссылки на команду.
+	 */
+	public constructor(targetObject: IUniversalObject)
+
+	/**
+	 * Конструктор команды Стоп. Останавливается определенная команда
 	 * @param stoppableCommandName Имя команды, которую надо остановить.
 	 * @param targetObject Объект, в котором будет производится поиск ссылки на команду.
 	 */
+	public constructor(stoppableCommandName: string, targetObject: IUniversalObject)
+
 	public constructor(
-		private stoppableCommandName: string,
-		private targetObject: IUniversalObject
-	) {}
+		stoppableCommandNameOrTargetObject: string | IUniversalObject,
+		targetObject?: IUniversalObject
+	) {
+		if (arguments.length === 1 && isUniversalObject(stoppableCommandNameOrTargetObject)) {
+			this.stoppableCommandName = undefined
+			this.targetObject = stoppableCommandNameOrTargetObject
+		} else if (typeof stoppableCommandNameOrTargetObject === 'string' && isUniversalObject(targetObject)) {
+			this.stoppableCommandName = stoppableCommandNameOrTargetObject
+			this.targetObject = targetObject
+		} else {
+			throw new Error('Задана неправильные параметры')
+		}
+	}
 
 	public execute(): void {
 		const stoppableCommandName = this.stoppableCommandName
 		const stoppableCommand = this.stoppableCommand
 		const stoppableMap = this.withStoppable.stoppableMap
 
-		if (!stoppableMap || !stoppableMap.has(stoppableCommandName) || !stoppableCommand) {
-			throw new Error(`Не найдена команда '${stoppableCommandName}'`)
+		if (!stoppableMap) {
+			throw new Error('Не найдена карта останавливаемых команд')
 		}
 
-		stoppableCommand.stop()
-		stoppableMap.delete(stoppableCommandName)
-	}
+		if (stoppableCommandName) {
+			// Останавливается одна определенная команда.
+			if (!stoppableMap.has(stoppableCommandName) || !stoppableCommand) {
+				throw new Error(`Не найдена команда '${stoppableCommandName}'`)
+			}
 
-	private get stoppableCommandFullName(): string {
-		return commandFullName(this.stoppableCommandName, this.stoppableCommand)
+			stoppableCommand.stop()
+			stoppableMap.delete(stoppableCommandName)
+		} else {
+			// Останавливаются все команды.
+			for (const [stoppableCommandName, stoppableCommand] of stoppableMap.entries()) {
+				stoppableCommand.stop()
+				stoppableMap.delete(stoppableCommandName)
+			}
+		}
 	}
 }
