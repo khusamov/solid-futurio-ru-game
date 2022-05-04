243ca0660d383104e9360fc07ad8accb5a8bc688
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-04 13:42:22 +0300

    Добавлен обработчик onCanvasResize()

--- packages/khusamov-game/src/components/Application/Application.tsx
+++ packages/khusamov-game/src/components/Application/Application.tsx
@@ -6,13 +6,32 @@ import GameObjectAdapter from '../../game/gameObject/GameObjectAdapter';
 import Canvas from '../Canvas';
 import Movable from '../Movable';
 import styles from './Application.module.scss'
+import {TOnResizeHandler} from '../Canvas/Canvas';
+import {IUniversalObject} from 'khusamov-universal-object';
+import {TTargetObjectSearchParams} from '../../game/order/IStartMoveTransformOrder';
+import ToroidalSurfaceAdapter from '../../game/gameObject/ToroidalSurfaceAdapter';
+
+const onCanvasResize: TOnResizeHandler = size => {
+	const theGameWorld = (
+		new ToroidalSurfaceAdapter(
+			resolve<IUniversalObject, [TTargetObjectSearchParams]>(
+				'GameObject',
+				{
+					type: 'GameObject',
+					name: 'theGameWorld'
+				}
+			)
+		)
+	)
+	theGameWorld.size = Object.assign({}, size)
+}
 
 export default function Application() {
 	useRequestAnimationFrame()
 	const gameObjectList = resolve<TGameObjectList>('GameObjectList')
 	return (
 		<div className={styles.Application}>
-			<Canvas>
+			<Canvas onResize={onCanvasResize}>
 				{gameObjectList.map(object => {
 					const gameObject = new GameObjectAdapter(object)
 					if (gameObject.kind && gameObject.kind.includes('IMovable')) {
