56b3eeee9df4eaa8dee4f5ca9d90951c40c12b84
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-04 12:33:36 +0300

    Исправлена работа хука useRequestAnimationFrame()

--- packages/khusamov-game/src/game/useRequestAnimationFrame.ts
+++ packages/khusamov-game/src/game/useRequestAnimationFrame.ts
@@ -1,12 +1,19 @@
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 export default function useRequestAnimationFrame() {
+	const [time, setTime] = useState(0)
+
 	const frame = useRef(0)
+
 	const render = (time: DOMHighResTimeStamp) => {
+		setTime(time)
 		frame.current = requestAnimationFrame(render)
 	}
+
 	useEffect(() => {
 		frame.current = requestAnimationFrame(render)
 		return () => cancelAnimationFrame(frame.current)
 	}, [])
+
+	return [time]
 }
