1cca576ea387148720af1ed2390d9651d56f2b4d
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-05 00:18:45 +0300

    Добавлено изображение корабля SpaceshipImage

--- packages/khusamov-game/src/components/Spaceship/Spaceship.tsx
+++ packages/khusamov-game/src/components/Spaceship/Spaceship.tsx
@@ -1,18 +1,52 @@
 import {MovableAdapter} from 'khusamov-mechanical-motion';
 import {IUniversalObject} from 'khusamov-universal-object';
+import SpaceshipImage from './SpaceshipImage.svg'
+import {Angle, ISize, Vector} from 'khusamov-base-types';
 
-const radius = 10
+const spaceshipImageSize: ISize = {
+	width: 21,
+	height: 38
+}
 
 interface ISpaceshipProps {
 	object: IUniversalObject
 }
 
 export default function Spaceship({object}: ISpaceshipProps) {
-	const {position} = new MovableAdapter(object)
+	const {position, linearVelocity} = new MovableAdapter(object)
 	const {x, y} = position
+	const transform = (
+		new Transform()
+			// Поворачиваем изображение корабля по вектору скорости.
+			// Считаем, что в редакторе нос корабля смотрит строго наверх.
+			.rotate(linearVelocity.rotate(Angle.toRadian(90)).angle)
+			// Смещение изображения по центру.
+			.translate(new Vector(spaceshipImageSize.width, spaceshipImageSize.height).scale(-1/2))
+	)
 	return (
 		<g transform={`translate(${x}, ${y})`}>
-			<circle r={radius}/>
+			<g transform={transform.toString()}>
+				<SpaceshipImage/>
+			</g>
 		</g>
 	)
+}
+
+class Transform {
+	private transforms: string[] = []
+	public scale(value: number): this {
+		this.transforms.push(`scale(${value})`)
+		return this
+	}
+	public rotate(angle: number): this {
+		this.transforms.push(`rotate(${Angle.toDegree(angle)})`)
+		return this
+	}
+	public translate({x, y}: Vector): this {
+		this.transforms.push(`translate(${x}, ${y})`)
+		return this
+	}
+	public toString(): string {
+		return this.transforms.join(', ')
+	}
 }
