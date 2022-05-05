88115494d3e2421d013679f9a7980676f50e3b9e
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-06 00:33:53 +0300

    Добавлена задача об удалении IStartMoveOrder

--- packages/khusamov-game/src/game/order/IStartMoveOrder.ts
+++ packages/khusamov-game/src/game/order/IStartMoveOrder.ts
@@ -5,6 +5,23 @@ import {IUniversalObject} from 'khusamov-universal-object';
 import {TTargetObjectSearchParams} from './IStartMoveTransformOrder';
 import {resolve} from 'khusamov-inversion-of-control';
 
+// TODO IStartMoveOrder надо заменить на IStarOrder
+// Чтобы вместо этого:
+// type: 'StartMoveTransform',
+// 	transformAction: 'IncreaseForce',
+// 	targetObject: {
+// 	type: 'GameObject',
+// 		name: targetObjectName
+// }
+// Стало это:
+// type: 'Start',
+// 	command: 'MoveTransform.IncreaseForce',
+// 	targetObject: {
+// 	type: 'GameObject',
+// 		name: targetObjectName
+// }
+// А то приказы старт стоп сейчас не соответствуют друг другу
+
 export default interface IStartMoveOrder extends IOrder {
 	type: 'StartMove',
 	targetObject: TTargetObjectSearchParams
@@ -28,6 +45,7 @@ export class StartMoveOrderAdapter implements IStartMoveOrder {
 
 export function startMoveCommandResolver(startMoveOrderObject: IUniversalObject): ICommand {
 	const startMoveOrder = new StartMoveOrderAdapter(startMoveOrderObject)
+
 	const targetObject = (
 		resolve<IUniversalObject | undefined>(
 			startMoveOrder.targetObject.type,
