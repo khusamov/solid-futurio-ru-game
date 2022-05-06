aee16f8cb14a720a6ce6d274dcafa80be7414fe7
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-06 13:04:40 +0300

    Уменьшить силу можно только до нуля

--- packages/khusamov-mechanical-motion/src/translational-motion/moveTransformAction/decreaseForceActionResolver.ts
+++ packages/khusamov-mechanical-motion/src/translational-motion/moveTransformAction/decreaseForceActionResolver.ts
@@ -1,12 +1,12 @@
+import {Vector} from 'khusamov-base-types';
 import IMovable from '../../IMovable';
 import {TMoveTransformAction} from '../MoveTransformCommand';
-import {Vector} from 'khusamov-base-types';
 
 export default function decreaseForceActionResolver(increment: number): TMoveTransformAction {
 	return (movable: IMovable) => {
 		movable.appliedForce = (
-			movable.appliedForce.isNull
-				? new Vector(-increment, 0)
+			movable.appliedForce.isNull || movable.appliedForce.length < increment
+				? new Vector(0, 0)
 				: movable.appliedForce.translate(movable.appliedForce.identity.scale(-increment))
 		)
 	}
