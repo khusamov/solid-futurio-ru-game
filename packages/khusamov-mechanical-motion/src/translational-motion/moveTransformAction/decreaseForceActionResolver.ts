aabe3940a86c6baf3fdaf3ad57d1ebed3774e356
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-06 12:20:55 +0300

    Исправлены действия по изменению длины приложенной силы

--- packages/khusamov-mechanical-motion/src/translational-motion/moveTransformAction/decreaseForceActionResolver.ts
+++ packages/khusamov-mechanical-motion/src/translational-motion/moveTransformAction/decreaseForceActionResolver.ts
@@ -1,6 +1,13 @@
 import IMovable from '../../IMovable';
 import {TMoveTransformAction} from '../MoveTransformCommand';
+import {Vector} from 'khusamov-base-types';
 
-export default function decreaseForceActionResolver(decrement: number): TMoveTransformAction {
-	return (movable: IMovable) => movable.appliedForce.length -= decrement
+export default function decreaseForceActionResolver(increment: number): TMoveTransformAction {
+	return (movable: IMovable) => {
+		movable.appliedForce = (
+			movable.appliedForce.isNull
+				? new Vector(-increment, 0)
+				: movable.appliedForce.translate(movable.appliedForce.identity.scale(-increment))
+		)
+	}
 }
--- packages/khusamov-mechanical-motion/src/translational-motion/moveTransformAction/increaseForceActionResolver.ts
+++ packages/khusamov-mechanical-motion/src/translational-motion/moveTransformAction/increaseForceActionResolver.ts
@@ -1,6 +1,13 @@
+import {Vector} from 'khusamov-base-types';
 import IMovable from '../../IMovable';
 import {TMoveTransformAction} from '../MoveTransformCommand';
 
 export default function increaseForceActionResolver(increment: number): TMoveTransformAction {
-	return (movable: IMovable) => movable.appliedForce.length += increment
+	return (movable: IMovable) => {
+		movable.appliedForce = (
+			movable.appliedForce.isNull
+				? new Vector(increment, 0)
+				: movable.appliedForce.translate(movable.appliedForce.identity.scale(increment))
+		)
+	}
 }
