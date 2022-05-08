8606416d034b723db8625e186d2203ddf65c1815
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-08 13:23:21 +0300

    Упрощен код

--- packages/khusamov-game/src/game/gameObject/ToroidalSurfaceAdapter.ts
+++ packages/khusamov-game/src/game/gameObject/ToroidalSurfaceAdapter.ts
@@ -2,14 +2,16 @@ import {ISize} from 'khusamov-base-types';
 import {IUniversalObject} from 'khusamov-universal-object';
 import IToroidalSurface from './IToroidalSurface';
 
+const NULL_SIZE: ISize = {
+	width: 0,
+	height: 0
+}
+
 export default class ToroidalSurfaceAdapter implements IToroidalSurface {
 	public constructor(private universalObject: IUniversalObject) {}
 
 	public get size(): ISize {
-		return this.universalObject.getValue('size', {
-			width: 0,
-			height: 0
-		})
+		return this.universalObject.getValue('size', NULL_SIZE)
 	}
 
 	public set size(value: ISize) {
