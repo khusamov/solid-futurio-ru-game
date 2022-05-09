ba2f0c70bb9f4d90d2adb3199d435b2ec331363f
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-09 17:16:27 +0300

    Добавлена поддержка пакетом khusamov-inversion-of-control выполнения кода в среде Node.js для модульного тестирования

--- packages/khusamov-inversion-of-control/src/index.ts
+++ packages/khusamov-inversion-of-control/src/index.ts
@@ -1,19 +1,22 @@
 export {default as IResolverContext} from './inversionOfControl/IResolverContext'
 export {default as register} from './inversionOfControl/register'
 export {default as resolve} from './inversionOfControl/resolve'
+export {TResolverFunction, TResolveParameters} from './inversionOfControl/types'
 
 // Проверка на повторную загрузку пакета.
 // Нельзя класс IoC определять более одного раза.
 
+const _global = typeof window === 'undefined' ? global : window
+
 const inversionOfControlPackageName = 'khusamov-inversion-of-control'
 const inversionOfControlPackageSymbol = Symbol.for(inversionOfControlPackageName)
 
-if (Reflect.get(window, inversionOfControlPackageSymbol) === inversionOfControlPackageName) {
+if (Reflect.get(_global, inversionOfControlPackageSymbol) === inversionOfControlPackageName) {
 	console.warn(`Пакет '${inversionOfControlPackageName}' загружен повторно!`)
 }
 
-if (Reflect.get(window, inversionOfControlPackageSymbol) !== inversionOfControlPackageName) {
-	Object.defineProperty(window, inversionOfControlPackageSymbol, {
+if (Reflect.get(_global, inversionOfControlPackageSymbol) !== inversionOfControlPackageName) {
+	Object.defineProperty(_global, inversionOfControlPackageSymbol, {
 		writable: true,
 		value: inversionOfControlPackageName
 	})
