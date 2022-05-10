8c43d38479714d5433378da2576d6aa8aadba368
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-10 23:51:58 +0300

    Создан приказ IMoveTransformCommandOrder

---
+++ packages/khusamov-game/src/orders/IMoveTransformCommandOrder.ts
@@ -0,0 +1,27 @@
+import {IRelayCommandOrder} from 'khusamov-command-order-system';
+import {
+	clockwiseRotateForceActionResolver,
+	counterclockwiseRotateForceActionResolver,
+	rotateForceActionResolver,
+	decreaseForceActionResolver,
+	increaseForceActionResolver,
+	toroidalTransformActionResolver
+} from 'khusamov-mechanical-motion';
+
+export type TTransformActionParams = (
+	| ['MoveTransformAction.UnknownTransformAction']
+	| ['MoveTransformAction.IncreaseForce', ...Parameters<typeof increaseForceActionResolver>]
+	| ['MoveTransformAction.DecreaseForce', ...Parameters<typeof decreaseForceActionResolver>]
+	| ['MoveTransformAction.RotateForce', ...Parameters<typeof rotateForceActionResolver>]
+	| ['MoveTransformAction.ToroidalPositionTransformation', ...Parameters<typeof toroidalTransformActionResolver>]
+	| ['MoveTransformAction.ClockwiseRotateForce', ...Parameters<typeof clockwiseRotateForceActionResolver>]
+	| ['MoveTransformAction.CounterclockwiseRotateForce', ...Parameters<typeof counterclockwiseRotateForceActionResolver>]
+)
+
+/**
+ * Приказ IMoveTransformCommandOrder.
+ * Обязательно сделать регистрацию:
+ * - register('MoveTransformCommand', relayCommandResolver)
+ */
+export default interface IMoveTransformCommandOrder
+	extends IRelayCommandOrder<TTransformActionParams, 'MoveTransformCommand'> {}
