1b10d17a344f55fca6f1afc2f729cd4e674eb2aa
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-10 20:33:20 +0300

    У функции createUniversalObject удален Partial для параметров создаваемого объекта

--- packages/khusamov-universal-object/src/functions/createUniversalObject.ts
+++ packages/khusamov-universal-object/src/functions/createUniversalObject.ts
@@ -6,6 +6,6 @@ import fillUniversalObject from './fillUniversalObject';
  * Создать IUniversalObject на основе plane-объекта.
  * @param object
  */
-export default function createUniversalObject<T extends object>(object: Partial<T>): UniversalObject {
+export default function createUniversalObject<T extends object>(object: T): UniversalObject {
 	return fillUniversalObject(new UniversalObject, object)
 }
