d84f4131e786bc4744cbeb8d930c4a6ed9bcd132
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-10 23:54:04 +0300

    Очередная бурная переделка игры

--- packages/khusamov-game/src/game/register.ts
+++ packages/khusamov-game/src/game/register.ts
@@ -1,86 +1,79 @@
 import {EventEmitter} from 'events';
-import {ISize, Queue, QueueLog, Timer, Vector} from 'khusamov-base-types';
+import {Timer, Vector} from 'khusamov-base-types';
 import {register, resolve} from 'khusamov-inversion-of-control';
 import {
 	createUniversalObject,
-	findUniversalObject,
-	IUniversalObject,
-	withoutType
+	IUniversalObject
 } from 'khusamov-universal-object';
 import {
 	createCommandQueue,
-	InterpretOrderCommand,
 	RepeatableCommand,
-	TCommandQueue,
-	TOrderQueue
+	TCommandQueue
 } from 'khusamov-command-system';
 import {
-	IMovable,
-	clockwiseRotateForceActionResolver,
-	counterclockwiseRotateForceActionResolver,
+	IMovable, ITransformable,
 	decreaseForceActionResolver,
 	increaseForceActionResolver,
-	toroidalTransformActionResolver, ITransformable
+	toroidalTransformActionResolver,
+	rotateForceActionResolver,
+	clockwiseRotateForceActionResolver,
+	counterclockwiseRotateForceActionResolver
 } from 'khusamov-mechanical-motion';
-import IStartMoveTransformOrder, {startMoveTransformCommandResolver, TTargetObjectSearchParams} from './order/IStartMoveTransformOrder';
-import IStartMoveOrder, {startMoveCommandResolver} from './order/IStartMoveOrder';
 import ToroidalSurfaceAdapter from './gameObject/ToroidalSurfaceAdapter';
-import {destroyCommandResolver} from './order/IDestroyOrder';
 import IGameObject from './gameObject/IGameObject';
 import IToroidalSurface from './gameObject/IToroidalSurface';
 import {TGameObjectList} from './types';
 import IRenderable from './gameObject/IRenderable';
-import createObjectKeyboardControl from './keyboardShortcut/createObjectKeyboardControl';
-import {stopCommandResolver} from './order/IStopOrder';
+import gameObjectResolver, {TGameObjectResolver} from './gameObjectResolver';
+import {
+	InterpretOrderCommand,
+	IStartCommandOrder,
+	relayCommandResolver,
+	startCommandResolver,
+	stopCommandResolver,
+	TOrderQueue
+} from 'khusamov-command-order-system';
+import IMoveCommandOrder, {moveCommandResolver} from '../orders/IMoveCommandOrder';
+import getGameWorldSize from './getGameWorldSize';
+import IMoveTransformCommandOrder from '../orders/IMoveTransformCommandOrder';
+import createOrderQueue from './createOrderQueue';
 
 const DEBUG = false
 const LOG = false
 
-register('Stop', stopCommandResolver)
-register('Destroy', destroyCommandResolver)
-register('StartMove', startMoveCommandResolver)
-register('StartMoveTransform', startMoveTransformCommandResolver)
+register('StartCommand', startCommandResolver)
+register('StopCommand', stopCommandResolver)
+register('RelayCommand', relayCommandResolver)
+register('MoveCommand', moveCommandResolver)
 register('MoveTransformAction.ToroidalPositionTransformation', toroidalTransformActionResolver)
 register('MoveTransformAction.ClockwiseRotateForce', clockwiseRotateForceActionResolver)
 register('MoveTransformAction.CounterclockwiseRotateForce', counterclockwiseRotateForceActionResolver)
+register('MoveTransformAction.RotateForce', rotateForceActionResolver)
 register('MoveTransformAction.DecreaseForce', decreaseForceActionResolver)
 register('MoveTransformAction.IncreaseForce', increaseForceActionResolver)
 
+register('MoveTransformCommand', relayCommandResolver)
+register('GameObject', gameObjectResolver)
+register('SelectedGameObject', () => resolve<IUniversalObject>('GameObject', 'theSpaceship'))
+
+const orderQueue: TOrderQueue = createOrderQueue(LOG)
 const commandQueue: TCommandQueue = createCommandQueue() // Очередь команд создается по особому!
 const commandQueueEventEmitter = new EventEmitter()
 const gameObjectList: TGameObjectList = []
 
-function createOrderQueue(): TOrderQueue {
-	if (LOG) {
-		const [orderQueue, orderQueueLog] = QueueLog.create<IUniversalObject>(new Queue)
-		Object.defineProperty(window, 'orderQueueLog', {value: orderQueueLog})
-		return orderQueue
-	} else {
-		return new Queue
-	}
-}
-const orderQueue: TOrderQueue = createOrderQueue()
-
-register('GameObjectList', () => gameObjectList)
 register('OrderQueue', () => orderQueue)
 register('CommandQueue', () => commandQueue)
 register('CommandQueue.EventEmitter', () => commandQueueEventEmitter)
+register('GameObjectList', () => gameObjectList)
 
-function gameObjectResolver(params: TTargetObjectSearchParams): IUniversalObject | undefined {
-	return findUniversalObject(
-		resolve<TGameObjectList>('GameObjectList'),
-		withoutType(params)
-	)
-}
-
-register('GameObject', gameObjectResolver)
+commandQueue.enqueue(new RepeatableCommand(new InterpretOrderCommand))
 
 /**
  * Генерация основных игровых объектов.
  */
 {
 	gameObjectList.push(
-		createUniversalObject<IGameObject & ITransformable>({
+		createUniversalObject<Partial<IGameObject & ITransformable>>({
 			name: 'theCamera',
 			kind: ['IGameObject', 'ITransformable']
 		})
@@ -90,21 +83,55 @@ register('GameObject', gameObjectResolver)
 		createUniversalObject<IGameObject & IToroidalSurface>({
 			name: 'theGameWorld',
 			kind: ['IGameObject', 'IToroidalSurface'],
-			size: {width: 3000, height: 3000}
+			size: {
+				width: 3000,
+				height: 3000
+			}
 		})
 	)
+}
 
+/**
+ * Генерация космического корабля.
+ */
+{
 	gameObjectList.push(
-		createUniversalObject<IGameObject & ITransformable & IMovable & IRenderable>({
+		createUniversalObject<Partial<IGameObject & ITransformable & IMovable & IRenderable>>({
 			name: 'theSpaceship',
 			kind: ['IGameObject', 'ITransformable', 'IMovable', 'IRenderable'],
 			renderComponent: 'Spaceship',
 			mass: 1000
 		})
 	)
+
+	type TStartMoveCommandOrder = IStartCommandOrder<TGameObjectResolver, IMoveCommandOrder<TGameObjectResolver>>
+	orderQueue.enqueue(
+		createUniversalObject<TStartMoveCommandOrder>({
+			type: 'StartCommand',
+			commandName: 'MoveCommand',
+			targetObject: ['GameObject', 'theSpaceship'],
+			command: {
+				type: 'MoveCommand',
+				targetObject: ['GameObject', 'theSpaceship']
+			}
+		})
+	)
+
+	type TStartMoveTransformCommandOrder = IStartCommandOrder<TGameObjectResolver, IMoveTransformCommandOrder>
+	orderQueue.enqueue(
+		createUniversalObject<TStartMoveTransformCommandOrder>({
+			type: 'StartCommand',
+			targetObject: ['GameObject', 'theSpaceship'],
+			commandName: 'MoveTransformAction.ToroidalPositionTransformation',
+			command: {
+				type: 'MoveTransformCommand',
+				name: 'MoveTransformAction.ToroidalPositionTransformation',
+				action: ['MoveTransformAction.ToroidalPositionTransformation', getGameWorldSize]
+			}
+		})
+	)
 }
 
-commandQueue.enqueue(new RepeatableCommand(new InterpretOrderCommand))
 
 /**
  * Генерация статических звезд.
@@ -116,17 +143,7 @@ commandQueue.enqueue(new RepeatableCommand(new InterpretOrderCommand))
 		return Math.floor(Math.random() * (max - min) + min) //The maximum is exclusive and the minimum is inclusive
 	}
 
-	const theGameWorld = (
-		new ToroidalSurfaceAdapter(
-			resolve<IUniversalObject, [TTargetObjectSearchParams]>(
-				'GameObject',
-				{
-					type: 'GameObject',
-					name: 'theGameWorld'
-				}
-			)
-		)
-	)
+	const theGameWorld = new ToroidalSurfaceAdapter(resolve<IUniversalObject>('GameObject', 'theGameWorld'))
 
 	Array(100)
 		.fill(1)
@@ -142,7 +159,7 @@ commandQueue.enqueue(new RepeatableCommand(new InterpretOrderCommand))
 		})
 		.forEach(({scale, position}, index) => {
 			gameObjectList.push(
-				createUniversalObject<IGameObject & ITransformable & IRenderable>({
+				createUniversalObject<Partial<IGameObject & ITransformable & IRenderable>>({
 					name: 'theStar' + index,
 					kind: ['IGameObject', 'ITransformable', 'IRenderable'],
 					renderComponent: 'Star',
@@ -177,74 +194,5 @@ if (DEBUG) {
 		})
 	))
 }
-
-orderQueue.enqueue(
-	createUniversalObject<IStartMoveOrder>({
-		type: 'StartMove',
-		targetObject: {
-			type: 'GameObject',
-			name: 'theSpaceship'
-		}
-	})
-)
-
-function getToroidalSurfaceSize(): ISize {
-	const theGameWorld = (
-		new ToroidalSurfaceAdapter(
-			resolve<IUniversalObject, [TTargetObjectSearchParams]>(
-				'GameObject',
-				{
-					type: 'GameObject',
-					name: 'theGameWorld'
-				}
-			)
-		)
-	)
-	return theGameWorld.size
-}
-
-orderQueue.enqueue(
-	createUniversalObject<IStartMoveTransformOrder>({
-		type: 'StartMoveTransform',
-		targetObject: {
-			type: 'GameObject',
-			name: 'theSpaceship'
-		},
-		transformAction: [
-			'ToroidalPositionTransformation',
-			getToroidalSurfaceSize
-		]
-	})
-)
-
-register('SelectedGameObject', () => (
-	resolve<IUniversalObject, [TTargetObjectSearchParams]>(
-		'GameObject',
-		{
-			type: 'GameObject',
-			name: 'theSpaceship'
-		}
-	)
-))
-
-createObjectKeyboardControl()
-
-
-// Демонстрация возможности уничтожения объектов.
-// document.addEventListener('keydown', event => {
-// 	if (event.code === 'KeyQ') {
-// 		orderQueue.enqueue(
-// 			createUniversalObject<IDestroyOrder>({
-// 				type: 'Destroy',
-// 				targetObject: {
-// 					type: 'GameObject',
-// 					name: 'theSpaceship'
-// 				}
-// 			})
-// 		)
-// 	}
-// })
-
-
 
 
