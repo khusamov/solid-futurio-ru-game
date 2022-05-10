ca5d1f2d3ce530ca3c320edf3b2fe074e213b074
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-10 23:51:19 +0300

    Добавлена функция gameObjectResolver()

---
+++ packages/khusamov-game/src/game/gameObjectResolver.ts
@@ -0,0 +1,14 @@
+import {findUniversalObject, IUniversalObject} from 'khusamov-universal-object';
+import {resolve} from 'khusamov-inversion-of-control';
+import {TGameObjectList} from './types';
+
+export type TGameObjectResolver = typeof gameObjectResolver
+
+export default function gameObjectResolver(gameObjectName: string): IUniversalObject | undefined {
+	return findUniversalObject(
+		resolve<TGameObjectList>('GameObjectList'),
+		{
+			name: gameObjectName
+		}
+	)
+}
