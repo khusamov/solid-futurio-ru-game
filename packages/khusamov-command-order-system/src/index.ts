379f52491c82cca444fd910fba013dd3e4013a0e
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-10 20:30:22 +0300

    Добавлен экспорт NotFoundTargetObjectError и NotDefineOrderTypeError

--- packages/khusamov-command-order-system/src/index.ts
+++ packages/khusamov-command-order-system/src/index.ts
@@ -1,5 +1,9 @@
+export {default as NotFoundTargetObjectError} from './errors/NotFoundTargetObjectError'
+export {default as NotDefineOrderTypeError} from './errors/NotDefineOrderTypeError'
+
 export {default as InterpretOrderCommand, TOrderQueue} from './InterpretOrderCommand'
 export {default as ICommandOrder, CommandOrderAdapter} from './orders/ICommandOrder'
+
 export {default as IRelayCommandOrder, RelayCommandOrderAdapter, relayCommandResolver} from './orders/IRelayCommandOrder'
 export {default as IMacroCommandOrder, MacroCommandOrderAdapter, macroCommandResolver} from './orders/IMacroCommandOrder'
 export {default as IStopCommandOrder, StopCommandOrderAdapter, stopCommandResolver} from './orders/IStopCommandOrder'
