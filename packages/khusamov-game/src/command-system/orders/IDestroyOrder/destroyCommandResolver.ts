c0dd1e8575ef0396f03c90b5207204d0de49be5d
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-12 07:04:07 +0300

    Класс DestroyCommand перенесен в директорию commands

--- packages/khusamov-game/src/command-system/orders/IDestroyOrder/destroyCommandResolver.ts
+++ packages/khusamov-game/src/command-system/orders/IDestroyOrder/destroyCommandResolver.ts
@@ -3,7 +3,7 @@ import {ICommand} from 'khusamov-base-types';
 import {resolve} from 'khusamov-inversion-of-control';
 import {NotFoundTargetObjectError} from 'khusamov-command-order-system';
 import DestroyOrderAdapter from './DestroyOrderAdapter';
-import DestroyCommand from './DestroyCommand';
+import DestroyCommand from '../../commands/DestroyCommand';
 
 export default function destroyCommandResolver(destroyOrderObject: IUniversalObject): ICommand {
 	const destroyOrder = new DestroyOrderAdapter(destroyOrderObject)
--- packages/khusamov-game/src/command-system/orders/IDestroyOrder/index.ts
+++ packages/khusamov-game/src/command-system/orders/IDestroyOrder/index.ts
@@ -1,4 +1,4 @@
 export {default} from './IDestroyOrder'
 export {default as DestroyOrderAdapter} from './DestroyOrderAdapter'
 export {default as destroyCommandResolver} from './destroyCommandResolver'
-export {default as DestroyCommand} from './DestroyCommand'
+export {default as DestroyCommand} from '../../commands/DestroyCommand'
