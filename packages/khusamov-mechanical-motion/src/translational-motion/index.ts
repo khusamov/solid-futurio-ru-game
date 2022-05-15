0d838dbcde7b7f90f7e371ea89c34134a464cf9c
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-15 16:36:31 +0300

    Исправлен экспорт khusamov-mechanical-motion/src/translational-motion

--- packages/khusamov-mechanical-motion/src/translational-motion/index.ts
+++ packages/khusamov-mechanical-motion/src/translational-motion/index.ts
@@ -1,5 +1,14 @@
 export {IncreaseForceCommand} from './IncreaseForceCommand'
+export {ToroidalTransformCommand} from './ToroidalTransformCommand'
+export {RotateForceCommand} from './RotateForceCommand'
+export {MoveCommand} from './MoveCommand'
 
 export {increaseForceCommandResolver} from './IncreaseForceCommand'
+export {toroidalTransformCommandResolver} from './ToroidalTransformCommand'
+export {rotateForceCommandResolver} from './RotateForceCommand'
+export {moveCommandResolver} from './MoveCommand'
 
 export type {IIncreaseForceCommandOrder} from './IncreaseForceCommand'
+export type {IToroidalTransformCommandOrder} from './ToroidalTransformCommand'
+export type {IRotateForceCommandOrder} from './RotateForceCommand'
+export type {IMoveCommandOrder} from './MoveCommand'
