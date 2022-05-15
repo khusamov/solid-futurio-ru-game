f52f25a1f6adb91b2738b5492b863cd5518c7d1c
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-16 02:30:05 +0300

    Исправлено вычисление вращательного движения

--- packages/khusamov-mechanical-motion/src/rotational-motion/RotateCommand/RotateCommand.ts
+++ packages/khusamov-mechanical-motion/src/rotational-motion/RotateCommand/RotateCommand.ts
@@ -17,8 +17,12 @@ export class RotateCommand implements ICommand {
 		if (this.fixedTimeInterval && timeInterval < this.fixedTimeInterval) return
 
 		const {mass, rotationalInertia, appliedForce, appliedForceRadius, rotation, angularAcceleration, angularVelocity} = this.rotable
+
+		// При вращении учитывается только проекция силы на направление движения объекта.
+		const force = appliedForce.project(rotation)
+
 		this.rotable.rotationalInertia = mass * appliedForceRadius * appliedForceRadius
-		this.rotable.angularAcceleration = appliedForce.length / rotationalInertia
+		this.rotable.angularAcceleration = force.length / rotationalInertia
 		this.rotable.angularVelocity = angularVelocity + angularAcceleration * Convert.toSecond(timeInterval)
 		this.rotable.rotation = rotation.rotate(angularVelocity * Convert.toSecond(timeInterval))
 
