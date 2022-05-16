165cd21448cd1c91e561e03f4666a7994d05e456
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-16 13:56:19 +0300

    Исправлено вычисление углового ускорения

--- packages/khusamov-mechanical-motion/src/rotational-motion/RotateCommand/RotateCommand.ts
+++ packages/khusamov-mechanical-motion/src/rotational-motion/RotateCommand/RotateCommand.ts
@@ -16,13 +16,24 @@ export class RotateCommand implements ICommand {
 		// Если фиксированный интервал задан, то выполняем вычисления не чаще этого значения.
 		if (this.fixedTimeInterval && timeInterval < this.fixedTimeInterval) return
 
-		const {mass, rotationalInertia, appliedForce, appliedForceRadius, rotation, angularAcceleration, angularVelocity} = this.rotable
+		const {mass, appliedForce, appliedForcePoint, rotation, angularAcceleration, angularVelocity} = this.rotable
+
+		// TODO Проверить appliedForcePoint
+		// Похоже вектор appliedForcePoint вычисляется на основании rotation и расстояния от position до точки приложения сил.
+		// Хотя нет, appliedForcePoint и appliedForce (точнее угол силы) вычисляются в другой команде для разных объектов по разному.
 
 		// При вращении учитывается только проекция силы на направление движения объекта.
-		const force = appliedForce.project(rotation)
+		// TODO Проверить, вполне возможно что можно вместо проекции саму силу и использовать.
+		const appliedForceProject = appliedForce.project(rotation)
 
-		this.rotable.rotationalInertia = mass * appliedForceRadius * appliedForceRadius
-		this.rotable.angularAcceleration = force.length / rotationalInertia
+		// Момент инерции (аналог массы для поступательного движения).
+		const rotationalInertia = mass * appliedForcePoint.length * appliedForcePoint.length
+
+		// Момент силы (аналог силы для поступательного движения).
+		const torque = appliedForcePoint.cross(appliedForceProject)
+
+
+		this.rotable.angularAcceleration = torque.scale(1 / rotationalInertia).z
 		this.rotable.angularVelocity = angularVelocity + angularAcceleration * Convert.toSecond(timeInterval)
 		this.rotable.rotation = rotation.rotate(angularVelocity * Convert.toSecond(timeInterval))
 
