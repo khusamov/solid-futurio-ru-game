cbda4242e9249068b7fbeeecb58f4e3aec2d12f0
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-16 13:55:52 +0300

    Команда IncreaseRadiusCommand удалена

--- packages/khusamov-mechanical-motion/src/rotational-motion/IncreaseRadiusCommand/index.ts
+++
@@ -1,3 +0,0 @@
-export {IncreaseRadiusCommand} from './IncreaseRadiusCommand'
-export {increaseRadiusCommandResolver} from './increaseRadiusCommandResolver'
-export type {IIncreaseRadiusCommandOrder} from './IIncreaseRadiusCommandOrder'
