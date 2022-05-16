e9fdf3b2c2eed443aadf20fb71e34e922d8cd47c
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-16 21:10:54 +0300

    Добавлена регистрация нового управления кораблем с клавиатуры

--- packages/khusamov-game/src/game/register/registerShortcuts.ts
+++ packages/khusamov-game/src/game/register/registerShortcuts.ts
@@ -1,11 +1,13 @@
 import {Convert, IDisposable} from 'khusamov-base-types';
 import {IIncreaseForceCommandOrder, IRotateForceCommandOrder} from 'khusamov-mechanical-motion';
 import IShortcutCommandOrder, {registerShortcutCommand} from '../../IShortcutCommandOrder';
+import {CobraEngine, IControlCobraSpaceshipCommandOrder} from '../../command-system/commands/ControlCobraSpaceshipCommand';
 
 type TSpaceshipShortcutCommand = (
 	| IShortcutCommandOrder<IIncreaseForceCommandOrder>
 	| IShortcutCommandOrder<IRotateForceCommandOrder>
-	)
+	| IShortcutCommandOrder<IControlCobraSpaceshipCommandOrder>
+)
 
 const increaseForceShortcut: TSpaceshipShortcutCommand = {
 	key: 'W',
@@ -59,11 +61,71 @@ const ccwRotateForceShortcut: TSpaceshipShortcutCommand = {
 	}
 }
 
+const increaseLeftEngineSpaceshipShortcut: TSpaceshipShortcutCommand = {
+	key: 'R',
+	command: {
+		name: 'ControlCobraSpaceshipCommand.IncreaseLeftEngine',
+		targetObject: ['SelectedGameObject'],
+		order: {
+			type: 'ControlCobraSpaceshipCommand',
+			targetObject: ['SelectedGameObject'],
+			engine: CobraEngine.Left,
+			increment: 200
+		}
+	}
+}
+
+const dereaseLeftEngineSpaceshipShortcut: TSpaceshipShortcutCommand = {
+	key: 'F',
+	command: {
+		name: 'ControlCobraSpaceshipCommand.DereaseLeftEngine',
+		targetObject: ['SelectedGameObject'],
+		order: {
+			type: 'ControlCobraSpaceshipCommand',
+			targetObject: ['SelectedGameObject'],
+			engine: CobraEngine.Left,
+			increment: -200
+		}
+	}
+}
+
+const increaseRightEngineSpaceshipShortcut: TSpaceshipShortcutCommand = {
+	key: 'T',
+	command: {
+		name: 'ControlCobraSpaceshipCommand.IncreaseRightEngine',
+		targetObject: ['SelectedGameObject'],
+		order: {
+			type: 'ControlCobraSpaceshipCommand',
+			targetObject: ['SelectedGameObject'],
+			engine: CobraEngine.Right,
+			increment: 200
+		}
+	}
+}
+
+const dereaseRightEngineSpaceshipShortcut: TSpaceshipShortcutCommand = {
+	key: 'G',
+	command: {
+		name: 'ControlCobraSpaceshipCommand.DereaseRightEngine',
+		targetObject: ['SelectedGameObject'],
+		order: {
+			type: 'ControlCobraSpaceshipCommand',
+			targetObject: ['SelectedGameObject'],
+			engine: CobraEngine.Right,
+			increment: -200
+		}
+	}
+}
+
 const shortcutCommandOrders: IShortcutCommandOrder[] = [
 	increaseForceShortcut,
 	decreaseForceShortcut,
 	cwRotateForceShortcut,
-	ccwRotateForceShortcut
+	ccwRotateForceShortcut,
+	increaseLeftEngineSpaceshipShortcut,
+	dereaseLeftEngineSpaceshipShortcut,
+	increaseRightEngineSpaceshipShortcut,
+	dereaseRightEngineSpaceshipShortcut
 ]
 
 export function registerShortcuts(): IDisposable[] {
