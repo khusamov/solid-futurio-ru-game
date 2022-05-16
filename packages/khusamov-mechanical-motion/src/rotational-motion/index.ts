755e53ba5e1c160405ee099832962cdb063cf871
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-16 21:04:30 +0300

    Экспорт команды IncreaseRadiusCommand удален

--- packages/khusamov-mechanical-motion/src/rotational-motion/index.ts
+++ packages/khusamov-mechanical-motion/src/rotational-motion/index.ts
@@ -1,8 +1,5 @@
 export {RotateCommand} from './RotateCommand'
-export {IncreaseRadiusCommand} from './IncreaseRadiusCommand'
 
 export {rotateCommandResolver} from './RotateCommand'
-export {increaseRadiusCommandResolver} from './IncreaseRadiusCommand'
 
 export type {IRotateCommandOrder} from './RotateCommand'
-export type {IIncreaseRadiusCommandOrder} from './IncreaseRadiusCommand'
