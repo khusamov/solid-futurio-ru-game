37037c49222e4c734cfc8107759ff1f7af538e41
Khusamov Sukhrob <khusamov@yandex.ru>
2022-05-20 16:09:32 +0300

    Удалены свойства IRigidBody.appliedForce и appliedForcePoint

--- packages/khusamov-mechanical-motion/src/interfaces/IRigidbody/IRigidBody.ts
+++ packages/khusamov-mechanical-motion/src/interfaces/IRigidbody/IRigidBody.ts
@@ -1,21 +1,7 @@
-import {Vector} from 'khusamov-base-types';
-
 export interface IRigidBody {
 	/**
 	 * Масса объекта.
 	 * Килограммы.
 	 */
 	mass: number
-
-	/**
-	 * Приложенная к центру масс сила.
-	 * Ньютоны.
-	 */
-	appliedForce: Vector
-
-	/**
-	 * Точка приложения силы.
-	 * Метры.
-	 */
-	appliedForcePoint: Vector
 }
